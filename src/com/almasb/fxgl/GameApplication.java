512404e605b3ec66ba323405f4d757f38aac6ea3
AlmasB <almaslvl@gmail.com>
2015-06-15 23:01:59 +0500

    added license header and missing documentation points

--- src/com/almasb/fxgl/GameApplication.java
+++ src/com/almasb/fxgl/GameApplication.java
@@ -1,3 +1,28 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.almasb.fxgl;
 
 import java.awt.image.BufferedImage;
@@ -107,6 +132,9 @@ public abstract class GameApplication extends Application {
      */
     protected MouseState mouse = new MouseState();
 
+    /**
+     * Used for loading various assets
+     */
     protected AssetManager assetManager = new AssetManager();
 
     /**
@@ -614,7 +642,7 @@ public abstract class GameApplication extends Application {
     /**
      * Register an FXGL event
      *
-     * Add specific entity types if the event should be targetting
+     * Add specific entity types if the event should be targeting
      * a group. Otherwise all entities will be notified of the event
      *
      * @param event
@@ -631,6 +659,11 @@ public abstract class GameApplication extends Application {
         }
     }
 
+    /**
+     * Saves a screenshot of the current main scene into a .png file
+     *
+     * @return  true if the screenshot was saved successfully, false otherwise
+     */
     protected boolean saveScreenshot() {
         Image fxImage = mainScene.snapshot(null);
         BufferedImage img = SwingFXUtils.fromFXImage(fxImage, null);
@@ -651,7 +684,16 @@ public abstract class GameApplication extends Application {
     }
 
     public static class MouseState {
+        /**
+         * Hold the value of x and y coordinate of the mouse cursor
+         * in the current frame (tick)
+         */
         public double x, y;
+
+        /**
+         * Hold the state of left and right
+         * mouse buttons in the current frame (tick)
+         */
         public boolean leftPressed, rightPressed;
         private MouseEvent event;
 
