8e4dba196a576635631abe375d23ff3ce2d41fe9
AlmasB <almaslvl@gmail.com>
2015-09-17 13:28:06 +0500

    added basic code samples

---
+++ samples/s1basicapp/BasicGameApplication.java
@@ -0,0 +1,74 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package s1basicapp;
+
+import com.almasb.fxgl.ApplicationMode;
+import com.almasb.fxgl.GameApplication;
+import com.almasb.fxgl.GameSettings;
+
+/**
+ * This is an example of a basic FXGL game application.
+ *
+ * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+ *
+ */
+public class BasicGameApplication extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Basic FXGL Application");
+        settings.setVersion("0.1developer");
+        settings.setFullScreen(false);
+        settings.setIntroEnabled(false);
+        settings.setMenuEnabled(false);
+        settings.setShowFPS(true);
+        settings.setApplicationMode(ApplicationMode.DEVELOPER);
+    }
+
+    @Override
+    protected void initAssets() throws Exception {}
+
+    @Override
+    protected void initGame() {}
+
+    @Override
+    protected void initPhysics() {}
+
+    @Override
+    protected void initUI() {}
+
+    @Override
+    protected void initInput() {}
+
+    @Override
+    protected void onUpdate() {}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
