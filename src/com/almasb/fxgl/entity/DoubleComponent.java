dd9cdf4f27fe60ba4c229d4c2574a9eac90a5b45
AlmasB <almaslvl@gmail.com>
2015-09-24 16:36:18 +0500

    added commonly used components

---
+++ src/com/almasb/fxgl/entity/DoubleComponent.java
@@ -0,0 +1,97 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.almasb.fxgl.entity;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+/**
+ * Represents a double value based component.
+ *
+ * <pre>
+ * Example:
+ *
+ * public class AttackSpeedComponent extends DoubleComponent {
+ *      public AttackSpeedComponent(double initialValue) {
+ *          super(initialValue);
+ *      }
+ * }
+ *
+ * Entity player = ...
+ * player.addComponent(new AttackSpeedComponent(1.75));
+ *
+ * double attackSpeed = player.getComponent(AttackSpeedComponent.class).getValue();
+ *
+ * </pre>
+ *
+ * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+ *
+ */
+public abstract class DoubleComponent implements Component {
+    private DoubleProperty property;
+
+    /**
+     * No-arg ctor, initializes the value to 0.
+     */
+    public DoubleComponent() {
+        this(0);
+    }
+
+    /**
+     * Constructs a double value component with given
+     * initial value.
+     *
+     * @param initialValue
+     */
+    public DoubleComponent(double initialValue) {
+        property = new SimpleDoubleProperty(initialValue);
+    }
+
+    /**
+     *
+     * @return value property
+     */
+    public final DoubleProperty valueProperty() {
+        return property;
+    }
+
+    /**
+     *
+     * @return value held by this component
+     */
+    public final double getValue() {
+        return property.get();
+    }
+
+    /**
+     * Set value to this component.
+     *
+     * @param value
+     */
+    public final void setValue(double value) {
+        property.set(value);
+    }
+}
