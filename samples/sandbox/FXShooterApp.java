ed0ae17b66204f95a99942d2e70bac83de6adade
AlmasB <almaslvl@gmail.com>
2016-01-23 00:26:59 +0000

    shooter test

---
+++ samples/sandbox/FXShooterApp.java
@@ -0,0 +1,104 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015-2016 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sandbox;
+
+import com.almasb.ents.AbstractControl;
+import com.almasb.ents.Entity;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.GameEntity;
+import com.almasb.fxgl.input.ActionType;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.InputMapping;
+import com.almasb.fxgl.input.OnUserAction;
+import com.almasb.fxgl.settings.GameSettings;
+import javafx.scene.input.KeyCode;
+
+/**
+ * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+ */
+public class FXShooterApp extends GameApplication {
+
+    GameEntity weapon;
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setTitle("FXShooterApp");
+        settings.setIntroEnabled(false);
+        settings.setMenuEnabled(false);
+    }
+
+    @Override
+    protected void initInput() {
+        Input input = getInput();
+
+        input.addInputMapping(new InputMapping("Shoot", KeyCode.F));
+    }
+
+    @Override
+    protected void initAssets() {
+
+    }
+
+    @Override
+    protected void initGame() {
+        weapon = new GameEntity();
+
+        weapon.addControl(new WeaponControl());
+    }
+
+    @Override
+    protected void initPhysics() {
+
+    }
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    @Override
+    protected void onUpdate() {
+
+    }
+
+    @OnUserAction(name = "Shoot", type = ActionType.ON_ACTION)
+    public void shoot() {
+        weapon.getControlUnsafe(WeaponControl.class).shoot();
+    }
+
+    public class WeaponControl extends AbstractControl {
+
+        public void shoot() {
+
+        }
+
+        @Override
+        public void onUpdate(Entity entity, double tpf) {
+
+        }
+    }
+}
