3afcbfd7b50c356e2fccfb4667c7abf8db11ed7d
AlmasB <almaslvl@gmail.com>
2016-02-06 17:52:42 +0000

    WIP for snap to objects window

--- src/main/java/com/almasb/fxgl/ui/InGameWindow.java
+++ src/main/java/com/almasb/fxgl/ui/InGameWindow.java
@@ -26,12 +26,20 @@
 
 package com.almasb.fxgl.ui;
 
+import com.almasb.fxgl.app.FXGL;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.ServiceType;
+import com.almasb.fxgl.scene.GameScene;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Paint;
 import jfxtras.scene.control.window.CloseIcon;
 import jfxtras.scene.control.window.MinimizeIcon;
 import jfxtras.scene.control.window.Window;
+
+import java.util.ArrayList;
 
 /**
  * Represents an in-game window as part of game UI.
@@ -48,6 +56,8 @@ import jfxtras.scene.control.window.Window;
  * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
  */
 public class InGameWindow extends Window {
+
+    private static final int SNAP_SIZE = 10;
 
     /**
      * Constructs an in-game window with minimize and close icons.
@@ -79,6 +89,71 @@ public class InGameWindow extends Window {
                 getRightIcons().addAll(new MinimizeIcon(this), new CloseIcon(this));
                 break;
         }
+
+        initXYListeners();
+    }
+
+    private void initXYListeners() {
+        double appW = FXGL.getDouble("settings.width");
+        double appH = FXGL.getDouble("settings.height");
+
+        ObservableList<Node> uiNodes = GameApplication.getService(ServiceType.GAME)
+                .getGameScene()
+                .getUINodes();
+
+        layoutXProperty().addListener((observable, oldValue, newValue) -> {
+            int newX = newValue.intValue();
+            int width = (int) getWidth();
+            int newMaxX = newX + width;
+
+            if (snapToWindows) {
+                for (Node node : new ArrayList<>(uiNodes)) {
+                    int nodeMaxX = (int) (node.getLayoutX() + node.getLayoutBounds().getWidth());
+
+                    if (between(newX, nodeMaxX - SNAP_SIZE, nodeMaxX + SNAP_SIZE)) {
+                        relocateX(nodeMaxX);
+                        break;
+                    }
+                }
+            }
+
+            if (snapToScreen) {
+                if (newX > 0 && newX <= SNAP_SIZE) {
+                    relocateX(0);
+                } else if (newMaxX < appW && newMaxX >= appW - SNAP_SIZE) {
+                    relocateX(appW - width);
+                }
+            }
+
+            if (canGoOffscreen)
+                return;
+
+            if (newX < 0)
+                relocateX(0);
+            else if (newMaxX > appW)
+                relocateX(appW - width);
+        });
+
+        layoutYProperty().addListener((observable, oldValue, newValue) -> {
+            int newY = newValue.intValue();
+            int newMaxY = newY + (int)getHeight();
+
+            if (snapToScreen) {
+                if (newY > 0 && newY <= SNAP_SIZE) {
+                    relocateY(0);
+                } else if (newMaxY < appH && newMaxY >= appH - SNAP_SIZE) {
+                    relocateY(appH - (int)getHeight());
+                }
+            }
+
+            if (canGoOffscreen)
+                return;
+
+            if (newY < 0)
+                relocateY(0);
+            else if (newMaxY > appH)
+                relocateY(appH - (int)getHeight());
+        });
     }
 
     /**
@@ -90,6 +165,44 @@ public class InGameWindow extends Window {
         setBackground(new Background(new BackgroundFill(color, null, null)));
     }
 
+    private boolean canGoOffscreen = false;
+
+    public final void setCanGoOffscreen(boolean value) {
+        canGoOffscreen = value;
+    }
+
+    public final boolean canGoOffscreen() {
+        return canGoOffscreen;
+    }
+
+    private boolean snapToScreen = true;
+
+    public final void setSnapToScreen(boolean value) {
+        snapToScreen = value;
+    }
+
+    public final boolean isSnapToScreen() {
+        return snapToScreen;
+    }
+
+    private boolean snapToWindows = true;
+
+    public final void setSnapToWindows(boolean value) {
+        snapToWindows = value;
+    }
+
+    public final boolean isSnapToWindows() {
+        return snapToWindows;
+    }
+
+    public final void relocateX(double x) {
+        setLayoutX(x - getLayoutBounds().getMinX());
+    }
+
+    public final void relocateY(double y) {
+        setLayoutY(y - getLayoutBounds().getMinY());
+    }
+
     /**
      * Set top-left position of the window.
      *
@@ -97,8 +210,11 @@ public class InGameWindow extends Window {
      * @param y top y
      */
     public final void setPosition(double x, double y) {
-        setTranslateX(x);
-        setTranslateY(y);
+        relocate(x, y);
+    }
+
+    private static boolean between(double value, double min, double max) {
+        return value > min && value < max;
     }
 
     public enum WindowDecor {
