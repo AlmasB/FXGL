9346ce8b7ae54f563490367b3ead34a204dcace4
AlmasB <almaslvl@gmail.com>
2016-03-13 21:50:59 +0000

    added automated unjar copy update

---
+++ deployment/update-javadoc-main.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# make dir desktop/tmp/javadoc if !exists
+
+# unjar there
+
+# update javadoc
+
+# remove dir
+
+mkdir -p ~/Desktop/tmp/javadoc
+
+cp $1 ~/Desktop/tmp/javadoc
+
+cd ~/Desktop/tmp/javadoc
+
+jar -xf *.jar
+
+rm *.jar
+
+cd ~/workspace/java/FXGL/deployment
+
+./update-javadoc.sh ~/Desktop/tmp/javadoc
