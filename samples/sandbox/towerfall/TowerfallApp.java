2655172bf8a559d725477cd265a2165fb8900261
AlmasB <almaslvl@gmail.com>
2016-12-03 20:31:48 +0000

    added basic towerfall sample

---
+++ samples/sandbox/towerfall/TowerfallApp.java
@@ -0,0 +1,155 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015-2016 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sandbox.towerfall;
+
+import com.almasb.ents.Entity;
+import com.almasb.fxgl.app.ApplicationMode;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.GameEntity;
+import com.almasb.fxgl.entity.component.CollidableComponent;
+import com.almasb.fxgl.entity.component.TypeComponent;
+import com.almasb.fxgl.gameplay.Level;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.parser.TextLevelParser;
+import com.almasb.fxgl.physics.CollisionHandler;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.settings.GameSettings;
+import javafx.geometry.Point2D;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public class TowerfallApp extends GameApplication {
+
+    private TowerfallFactory factory = new TowerfallFactory();
+    private GameEntity player;
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(1280);
+        settings.setHeight(720);
+        settings.setTitle("Towerfall");
+        settings.setVersion("0.1");
+        settings.setFullScreen(false);
+        settings.setIntroEnabled(false);
+        settings.setMenuEnabled(false);
+        settings.setProfilingEnabled(false);
+        settings.setApplicationMode(ApplicationMode.DEVELOPER);
+    }
+
+    @Override
+    protected void initInput() {
+        Input input = getInput();
+
+        input.addAction(new UserAction("Left") {
+            @Override
+            protected void onAction() {
+                double dy = player.getComponentUnsafe(PhysicsComponent.class).getLinearVelocity().getY();
+
+                player.getComponentUnsafe(PhysicsComponent.class).setLinearVelocity(new Point2D(-150, dy));
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Right") {
+            @Override
+            protected void onAction() {
+                double dy = player.getComponentUnsafe(PhysicsComponent.class).getLinearVelocity().getY();
+
+                player.getComponentUnsafe(PhysicsComponent.class).setLinearVelocity(new Point2D(150, dy));
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Jump") {
+            @Override
+            protected void onActionBegin() {
+                double dx = player.getComponentUnsafe(PhysicsComponent.class).getLinearVelocity().getX();
+
+                player.getComponentUnsafe(PhysicsComponent.class).setLinearVelocity(new Point2D(dx, -300));
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Shoot") {
+            @Override
+            protected void onActionBegin() {
+                double x = player.getX();
+                double y = player.getY();
+
+                Point2D velocity = input.getMousePositionWorld()
+                        .subtract(x, y)
+                        .normalize()
+                        .multiply(500);
+
+                getGameWorld().addEntity(factory.newArrow((int) x + 40, (int) y, velocity));
+            }
+        }, MouseButton.PRIMARY);
+    }
+
+    @Override
+    protected void initAssets() {}
+
+    @Override
+    protected void initGame() {
+        TextLevelParser parser = new TextLevelParser(factory);
+        Level level = parser.parse("towerfall_level.txt");
+
+        player = (GameEntity) level.getEntities()
+                .stream()
+                .filter(e -> e.hasComponent(TypeComponent.class))
+                .filter(e -> e.getComponentUnsafe(TypeComponent.class).isType(EntityType.PLAYER))
+                .findAny()
+                .get();
+
+        getGameWorld().setLevel(level);
+    }
+
+    @Override
+    protected void initPhysics() {
+        getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityType.ARROW, EntityType.PLATFORM) {
+            @Override
+            protected void onCollisionBegin(Entity arrow, Entity platform) {
+                // necessary since we can collide with two platforms in the same frame
+                if (arrow.hasControl(ArrowControl.class)) {
+                    arrow.getComponentUnsafe(CollidableComponent.class).setValue(false);
+                    arrow.removeControl(ArrowControl.class);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void initUI() {}
+
+    @Override
+    protected void onUpdate(double tpf) {}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ samples/sandbox/towerfall/TowerfallFactory.java
@@ -0,0 +1,106 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015-2016 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sandbox.towerfall;
+
+import com.almasb.ents.Entity;
+import com.almasb.fxgl.app.FXGL;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.component.CollidableComponent;
+import com.almasb.fxgl.entity.control.OffscreenCleanControl;
+import com.almasb.fxgl.parser.EntityFactory;
+import com.almasb.fxgl.parser.EntityProducer;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.gameutils.math.GameMath;
+import com.almasb.gameutils.math.Vec2;
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public class TowerfallFactory extends EntityFactory {
+
+    public TowerfallFactory() {
+        super('0');
+    }
+
+    @EntityProducer('1')
+    public Entity newPlatform(int x, int y) {
+        return Entities.builder()
+                .type(EntityType.PLATFORM)
+                .at(x * 40, y * 40)
+                .viewFromNodeWithBBox(FXGL.getAssetLoader().loadTexture("brick.png", 40, 40))
+                .with(new CollidableComponent(true))
+                .with(new PhysicsComponent())
+                .build();
+    }
+
+    @EntityProducer('P')
+    public Entity newPlayer(int x, int y) {
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.DYNAMIC);
+
+        return Entities.builder()
+                .type(EntityType.PLAYER)
+                .at(x * 40, y * 40)
+                .viewFromNode(new Rectangle(36, 36, Color.BLUE))
+                .bbox(new HitBox("Main", BoundingShape.circle(18)))
+                .with(physics)
+                .build();
+    }
+
+    public Entity newArrow(int x, int y, Point2D velocity) {
+//        PhysicsComponent physics = new PhysicsComponent();
+//        physics.setOnPhysicsInitialized(() -> physics.setLinearVelocity(velocity));
+//
+//        FixtureDef fixtureDef = new FixtureDef();
+//        fixtureDef.setDensity(0.03f);
+//
+//
+//
+//        BodyDef bodyDef = new BodyDef();
+//        bodyDef.setType(BodyType.DYNAMIC);
+//        bodyDef.setAngle(-new Vec2((float)velocity.getX(), (float)velocity.getY()).angle());
+//
+//        physics.setFixtureDef(fixtureDef);
+//        physics.setBodyDef(bodyDef);
+
+        return Entities.builder()
+                .type(EntityType.ARROW)
+                .at(x, y)
+                .viewFromNodeWithBBox(new Rectangle(15, 2, Color.RED))
+                .with(new CollidableComponent(true))
+                .with(new OffscreenCleanControl(), new ArrowControl(velocity.normalize()))
+                .build();
+    }
+}
