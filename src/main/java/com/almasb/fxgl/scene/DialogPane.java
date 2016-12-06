5b8753ae4b7e348583d37f395c618bf037aa3b95
AlmasB <almaslvl@gmail.com>
2016-12-06 19:19:38 +0000

    removed old code

--- src/main/java/com/almasb/fxgl/scene/DialogPane.java
+++ src/main/java/com/almasb/fxgl/scene/DialogPane.java
@@ -64,8 +64,6 @@ import java.util.function.Predicate;
  */
 public class DialogPane extends Pane {
 
-    //private static final Logger log = FXGLLoggerOld.getLogger("FXGL.DialogPane");
-
     public static final Predicate<String> ALPHANUM = input -> input.matches("^[\\pL\\pN]+$");
 
     private Window window = new Window();
