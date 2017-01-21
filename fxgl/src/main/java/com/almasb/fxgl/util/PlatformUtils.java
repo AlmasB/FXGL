9f6cb156f5a6105126dfe8639ed7c16c1d883489
Stefan Banu <stefanbanu1@gmail.com>
2017-01-21 22:44:33 +0200

    added changes on methods call

--- fxgl/src/main/java/com/almasb/fxgl/util/PlatformUtils.java
+++ fxgl/src/main/java/com/almasb/fxgl/util/PlatformUtils.java
@@ -5,13 +5,35 @@ package com.almasb.fxgl.util;
  */
 public class PlatformUtils {
 
-    // Operation System name
-    public static String getOsString() {
-        return  System.getProperty("os.name");
+    private static String OS = null;
+
+    public static String getOsName() {
+        if(OS == null) { OS = System.getProperty("os.name"); }
+        return OS.toLowerCase();
+    }
+
+    public static boolean isWindows() {
+        return getOsName().contains("win");
+    }
+
+    public static boolean isUnix(){
+        return getOsName().contains("nix");
+    }
+
+    public static boolean isMac(){
+        return getOsName().contains("mac");
+    }
+
+    public static boolean isLinux(){
+        return getOsName().contains("nux");
+    }
+
+    public static boolean isSolaris(){
+        return getOsName().contains("sunos");
     }
 
     // Operation System version
-    public static String getJavaOsVersionString() {
+    public static String getOsVersionString() {
         return System.getProperty("os.version");
     }
 
