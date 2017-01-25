7de61d33076889ea96aedbf2656ace2754e02052
AlmasB <almaslvl@gmail.com>
2017-01-25 00:10:51 +0000

    added ReflectionUtils

---
+++ fxgl-core/src/main/java/com/almasb/fxgl/core/reflect/ReflectionUtils.java
@@ -0,0 +1,82 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015-2017 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.almasb.fxgl.core.reflect;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public final class ReflectionUtils {
+
+    private ReflectionUtils() {}
+
+    public static <A extends java.lang.annotation.Annotation> Map<A, Method> findMethods(Object instance, Class<A> annotationClass) {
+        Map<A, Method> map = new HashMap<>();
+
+        for (java.lang.reflect.Method method : instance.getClass().getDeclaredMethods()) {
+            A annotation = method.getDeclaredAnnotation(annotationClass);
+            if (annotation != null) {
+                map.put(annotation, method);
+            }
+        }
+
+        return map;
+    }
+
+    public static <T, R, A extends java.lang.annotation.Annotation> Map<A, Function<T, R>> findMethodsMapFunctions(Object instance, Class<A> annotationClass) {
+        Map<A, Function<T, R>> map = new HashMap<>();
+
+        for (java.lang.reflect.Method method : instance.getClass().getDeclaredMethods()) {
+            A annotation = method.getDeclaredAnnotation(annotationClass);
+            if (annotation != null) {
+                map.put(annotation, mapToFunction(instance, method));
+            }
+        }
+
+        return map;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T call(Object instance, Method method, Object... args) {
+        try {
+            return (T) method.invoke(instance, args);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot call " + method.getName() + " Error: " + e);
+        }
+    }
+
+    public static <T, R> Function<T, R> mapToFunction(Object instance, Method method) {
+        return input -> call(instance, method, input);
+    }
+}
