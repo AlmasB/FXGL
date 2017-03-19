24df34687e8cbf9d23e37acf5e70b05429ea804f
AlmasB <almaslvl@gmail.com>
2017-03-19 20:21:20 +0000

    more sophisticated sample

--- fxgl-samples/src/main/java/s10miscellaneous/ViewportSample.java
+++ fxgl-samples/src/main/java/s10miscellaneous/ViewportSample.java
@@ -33,12 +33,16 @@ import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.core.math.Vec2;
 import com.almasb.fxgl.entity.Entities;
 import com.almasb.fxgl.entity.GameEntity;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.parser.text.TextLevelParser;
 import com.almasb.fxgl.scene.Viewport;
 import com.almasb.fxgl.settings.GameSettings;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.geometry.Point2D;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.util.Duration;
@@ -52,48 +56,85 @@ public class ViewportSample extends GameApplication {
 
     @Override
     protected void initSettings(GameSettings settings) {
-        settings.setWidth(800);
+        settings.setWidth(1600);
         settings.setHeight(600);
         settings.setTitle("ViewportSample");
         settings.setVersion("0.1");
         settings.setFullScreen(false);
         settings.setIntroEnabled(false);
         settings.setMenuEnabled(false);
-        settings.setProfilingEnabled(true);
+        settings.setProfilingEnabled(false);
         settings.setCloseConfirmation(false);
         settings.setApplicationMode(ApplicationMode.DEVELOPER);
     }
 
     @Override
+    protected void initInput() {
+        getInput().addAction(new UserAction("Move Viewport Up") {
+            @Override
+            protected void onAction() {
+                getGameScene().getViewport().setY(getGameScene().getViewport().getY() - 5);
+            }
+        }, KeyCode.W);
+
+        getInput().addAction(new UserAction("Move Viewport Down") {
+            @Override
+            protected void onAction() {
+                getGameScene().getViewport().setY(getGameScene().getViewport().getY() + 5);
+            }
+        }, KeyCode.S);
+
+        getInput().addAction(new UserAction("Print Coords") {
+            @Override
+            protected void onActionBegin() {
+                //System.out.println(getInput().getMousePositionWorld());
+                //cinematicViewport();
+                cinematicViewportBezier();
+            }
+        }, MouseButton.PRIMARY);
+    }
+
+    @Override
     protected void initGame() {
-        Entities.builder()
-                .at(0, 0)
-                .viewFromNode(new Rectangle(40, 40, Color.BLUE))
-                .buildAndAttach(getGameWorld());
+        TextLevelParser parser = new TextLevelParser('0', 40, 40);
+        parser.addEntityProducer('1', data -> Entities.builder()
+                .from(data)
+                .viewFromNode(new Rectangle(40, 40, Color.color(FXGLMath.random(), FXGLMath.random(), FXGLMath.random())))
+                .build()
+        );
 
-        Entities.builder()
-                .at(800, 0)
-                .viewFromNode(new Rectangle(40, 40, Color.RED))
-                .buildAndAttach(getGameWorld());
+        getGameWorld().setLevel(parser.parse("level_viewport.txt"));
+    }
 
-        Entities.builder()
-                .at(600, 560)
-                .viewFromNode(new Rectangle(40, 40, Color.GREEN))
-                .buildAndAttach(getGameWorld());
+    @Override
+    protected void initUI() {
+        Circle circle = new Circle(15, Color.RED);
+        circle.setTranslateX(getWidth() / 2);
+        circle.setTranslateY(getHeight() / 2);
 
-        cinematicViewportBezier();
+        getGameScene().addUINode(circle);
     }
 
     private void cinematicViewport() {
         // 1. get viewport
         Viewport viewport = getGameScene().getViewport();
-        viewport.setX(-150);
+        viewport.setX(-getWidth() / 2);
+        viewport.setY(-getHeight() / 2);
 
         // 2. define "waypoints"
         Point2D[] points = {
-                new Point2D(800, 0),
-                new Point2D(300, 300),
-                new Point2D(0, 0)
+                new Point2D(98, 80),
+                new Point2D(1520, 90),
+                new Point2D(1470, 272),
+                new Point2D(171, 293),
+                new Point2D(154, 485),
+                new Point2D(1500, 483),
+                new Point2D(1474, 683),
+                new Point2D(144, 698),
+                new Point2D(161, 892),
+                new Point2D(1475, 888),
+                new Point2D(1477, 1064),
+                new Point2D(108, 1066)
         };
 
         Timeline timeline = new Timeline();
@@ -101,11 +142,11 @@ public class ViewportSample extends GameApplication {
         int i = 0;
         for (Point2D p : points) {
             // bind viewport property values to pre-defined points
-            KeyValue kv = new KeyValue(viewport.xProperty(), p.getX());
-            KeyValue kv2 = new KeyValue(viewport.yProperty(), p.getY());
+            KeyValue kv = new KeyValue(viewport.xProperty(), p.getX() - getWidth() / 2);
+            KeyValue kv2 = new KeyValue(viewport.yProperty(), p.getY() - getHeight() / 2);
 
             // create frame
-            KeyFrame frame = new KeyFrame(Duration.seconds(3 * ++i), kv, kv2);
+            KeyFrame frame = new KeyFrame(Duration.seconds(2 * ++i), kv, kv2);
             timeline.getKeyFrames().add(frame);
         }
 
@@ -116,18 +157,29 @@ public class ViewportSample extends GameApplication {
     private void cinematicViewportBezier() {
         // 1. get viewport
         Viewport viewport = getGameScene().getViewport();
+        viewport.setX(-getWidth() / 2);
+        viewport.setY(-getHeight() / 2);
 
         // 2. define "waypoints"
         Vec2[] points = {
-                new Vec2(0, 0),
-                new Vec2(800, 0),
-                new Vec2(300, 300)
+                new Vec2(98, 80),
+                new Vec2(1520, 90),
+                new Vec2(1470, 272),
+                new Vec2(171, 293),
+                new Vec2(154, 485),
+                new Vec2(1500, 483),
+                new Vec2(1474, 683),
+                new Vec2(144, 698),
+                new Vec2(161, 892),
+                new Vec2(1475, 888),
+                new Vec2(1477, 1064),
+                new Vec2(108, 1066)
         };
 
         BezierSpline spline = FXGLMath.closedBezierSpline(points);
 
         Path path = new Path();
-        path.getElements().add(new MoveTo(0, 0));
+        path.getElements().add(new MoveTo(98, 80));
 
         for (BezierSpline.BezierCurve c : spline.getCurves()) {
             path.getElements().add(new CubicCurveTo(
@@ -138,17 +190,17 @@ public class ViewportSample extends GameApplication {
         }
 
         // if open bezier is needed
-        //path.getElements().remove(path.getElements().size()-1);
+        path.getElements().remove(path.getElements().size()-1);
 
-        GameEntity e = Entities.builder()
+        GameEntity camera = Entities.builder()
                 .build();
 
-        viewport.xProperty().bind(e.getPositionComponent().xProperty());
-        viewport.yProperty().bind(e.getPositionComponent().yProperty());
+        viewport.xProperty().bind(camera.getPositionComponent().xProperty().subtract(getWidth() / 2));
+        viewport.yProperty().bind(camera.getPositionComponent().yProperty().subtract(getHeight() / 2));
 
         Entities.animationBuilder()
-                .duration(Duration.seconds(9))
-                .translate(e)
+                .duration(Duration.seconds(24))
+                .translate(camera)
                 .alongPath(path)
                 .buildAndPlay();
     }
