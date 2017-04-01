5efdb108963a12c550ae9da9be1e0171b0dba18d
AlmasB <almaslvl@gmail.com>
2017-04-01 16:52:41 +0100

    FSMControl WIP

---
+++ fxgl-samples/src/main/java/sandbox/rts/FSMControl.java
@@ -0,0 +1,58 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015-2017 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sandbox.rts;
+
+import com.almasb.fxgl.ecs.AbstractControl;
+import com.almasb.fxgl.ecs.Entity;
+import com.badlogic.gdx.ai.fsm.DefaultStateMachine;
+import com.badlogic.gdx.ai.fsm.State;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public class FSMControl<T extends Entity> extends AbstractControl {
+
+    private DefaultStateMachine<T, State<T>> fsm;
+
+    public <S extends State<T>> FSMControl(Class<S> stateType, S initialState) {
+        fsm = new DefaultStateMachine<>(null, initialState);
+    }
+
+    @Override
+    public void onAdded(Entity entity) {
+        fsm.setOwner((T) entity);
+    }
+
+    @Override
+    public void onUpdate(Entity entity, double tpf) {
+        fsm.update();
+    }
+
+    public void changeState(State<T> newState) {
+        fsm.changeState(newState);
+    }
+}
--- fxgl-samples/src/main/java/sandbox/rts/RTSSample.java
+++ fxgl-samples/src/main/java/sandbox/rts/RTSSample.java
@@ -27,7 +27,11 @@
 package sandbox.rts;
 
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.settings.GameSettings;
+import javafx.scene.input.MouseButton;
+
+import java.util.Map;
 
 /**
  *
@@ -48,7 +52,27 @@ public class RTSSample extends GameApplication {
         settings.setProfilingEnabled(false);
     }
 
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("gold", 0);
+    }
 
+    @Override
+    protected void initInput() {
+        getInput().addAction(new UserAction("Spawn Worker") {
+            @Override
+            protected void onActionBegin() {
+                getGameWorld().spawn("Worker", getInput().getMousePositionWorld());
+            }
+        }, MouseButton.PRIMARY);
+    }
+
+    @Override
+    protected void initGame() {
+        getGameWorld().spawn("TownHall", 500, 100);
+        getGameWorld().spawn("GoldMine", 100, 100);
+        getGameWorld().spawn("GoldMine", 500, 400);
+    }
 
     public static void main(String[] args) {
         launch(args);
