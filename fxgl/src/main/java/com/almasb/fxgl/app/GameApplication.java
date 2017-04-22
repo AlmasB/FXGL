a27fc1ac7dd2fb515f5c4295a1125431a02aea57
AlmasB <almaslvl@gmail.com>
2017-04-22 14:54:32 +0100

    merged files to enhance flow readability

--- fxgl/src/main/java/com/almasb/fxgl/app/GameApplication.java
+++ fxgl/src/main/java/com/almasb/fxgl/app/GameApplication.java
@@ -27,26 +27,41 @@ package com.almasb.fxgl.app;
 
 import com.almasb.fxgl.asset.FXGLAssets;
 import com.almasb.fxgl.core.concurrent.Async;
+import com.almasb.fxgl.core.logging.FXGLLogger;
+import com.almasb.fxgl.core.logging.Logger;
 import com.almasb.fxgl.devtools.profiling.Profiler;
 import com.almasb.fxgl.entity.GameWorld;
+import com.almasb.fxgl.gameplay.AchievementManager;
 import com.almasb.fxgl.gameplay.GameState;
 import com.almasb.fxgl.io.FXGLIO;
 import com.almasb.fxgl.physics.PhysicsWorld;
 import com.almasb.fxgl.saving.DataFile;
 import com.almasb.fxgl.scene.DisplayEvent;
 import com.almasb.fxgl.scene.GameScene;
+import com.almasb.fxgl.scene.PreloadingScene;
 import com.almasb.fxgl.scene.SceneFactory;
 import com.almasb.fxgl.scene.menu.MenuEventListener;
-import com.almasb.fxgl.service.Input;
+import com.almasb.fxgl.service.*;
 import com.almasb.fxgl.service.impl.timer.FPSCounter;
+import com.almasb.fxgl.settings.GameSettings;
+import com.almasb.fxgl.settings.ReadOnlyGameSettings;
+import com.almasb.fxgl.util.Version;
 import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyLongWrapper;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
@@ -56,7 +71,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * <ol>
  * <li>Instance fields of YOUR subclass of GameApplication</li>
  * <li>initSettings()</li>
- * <li>Services configuration (after this you can safely call FXGL.getService())</li>
+ * <li>Services configuration (after this you can safely call any FXGL.* methods)</li>
  * <li>initAchievements()</li>
  * <li>initInput()</li>
  * <li>preInit()</li>
@@ -66,40 +81,147 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * <li>initGame() OR loadState()</li>
  * <li>initPhysics()</li>
  * <li>initUI()</li>
- * <li>Start of main game loop execution on UI thread</li>
+ * <p>Start of main game loop execution on UI thread</p>
  * </ol>
  * <p>
  * Unless explicitly stated, methods are not thread-safe and must be
  * executed on the JavaFX Application (UI) Thread.
  * By default all callbacks are executed on the JavaFX Application (UI) Thread.
- * <p>
- *     Callback / listener notes: instance of GameApplication will always be
- *     notified last along the chain of callbacks.
- *     However, as per documentation, events are always fired after listeners.
- * </p>
  *
  * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
  */
-public abstract class GameApplication extends SimpleFXGLApplication {
+public abstract class GameApplication extends Application {
 
+    /**
+     * Use system logger until actual logger is ready.
+     */
+    private static Logger log = FXGLLogger.getSystemLogger();
+
+    private Stage primaryStage;
+    private ReadOnlyGameSettings settings;
     private AppStateMachine stateMachine;
-
-    public final AppStateMachine getStateMachine() {
-        return stateMachine;
-    }
-
     private SceneFactory sceneFactory;
 
-    public SceneFactory getSceneFactory() {
-        return sceneFactory;
-    }
-
-    public void setSceneFactory(SceneFactory sceneFactory) {
-        this.sceneFactory = sceneFactory;
-    }
-
+    /**
+     * This is the main entry point as run by the JavaFX platform.
+     */
     @Override
-    void configureApp() {
+    public final void start(Stage stage) {
+        primaryStage = stage;
+
+        Version.print();
+        showPreloadingStage();
+        startFXGL();
+    }
+
+    /**
+     * Shows preloading stage with scene while FXGL is being configured.
+     */
+    private void showPreloadingStage() {
+        Stage preloadingStage = new Stage(StageStyle.UNDECORATED);
+        preloadingStage.initOwner(primaryStage);
+        preloadingStage.setScene(new PreloadingScene());
+        preloadingStage.show();
+
+        // when main stage is ready to show
+        primaryStage.setOnShowing(e -> {
+            // close our preloader
+            preloadingStage.close();
+            // clean the reference to lambda + preloader
+            primaryStage.setOnShowing(null);
+        });
+    }
+
+    private void startFXGL() {
+        new Thread(() -> {
+            try {
+                configureFXGL();
+
+                runUpdaterAndWait();
+
+                configureApp();
+            } catch (Exception e) {
+                log.fatal("Exception during system configuration:");
+                log.fatal(FXGLLogger.errorTraceAsString(e));
+                log.fatal("System will now exit");
+                log.close();
+
+                // we don't know what exactly has been initialized
+                // so to avoid the process hanging just shut down the JVM
+                System.exit(-1);
+            }
+        }, "FXGL Launcher Thread").start();
+    }
+
+    private void runTask(Class<? extends Runnable> type) {
+        log.debug("Running task: " + type.getSimpleName());
+        FXGL.getInstance(type).run();
+    }
+
+    private void runUpdaterAndWait() {
+        Async.startFX(() -> runTask(UpdaterTask.class)).await();
+    }
+
+    /**
+     * After this call all FXGL.* calls are valid.
+     */
+    private void configureFXGL() {
+        long start = System.nanoTime();
+
+        initSystemProperties();
+        initUserProperties();
+        initAppSettings();
+
+        FXGL.configure(new ApplicationModule(this));
+
+        // actual logger is ready
+        log = FXGLLogger.get(GameApplication.class);
+        log.debug("FXGL configuration complete");
+
+        log.infof("FXGL configuration took:  %.3f sec", (System.nanoTime() - start) / 1000000000.0);
+
+        log.debug("Logging game settings\n" + settings.toString());
+    }
+
+    /**
+     * Load FXGL system properties.
+     */
+    private void initSystemProperties() {
+        ResourceBundle props = ResourceBundle.getBundle("com.almasb.fxgl.app.system");
+        props.keySet().forEach(key -> {
+            Object value = props.getObject(key);
+            FXGL.setProperty(key, value);
+        });
+    }
+
+    /**
+     * Load user defined properties to override FXGL system properties.
+     */
+    private void initUserProperties() {
+        // services are not ready yet, so load manually
+        try (InputStream is = getClass().getResource("/assets/properties/system.properties").openStream()) {
+            ResourceBundle props = new PropertyResourceBundle(is);
+            props.keySet().forEach(key -> {
+                Object value = props.getObject(key);
+                FXGL.setProperty(key, value);
+            });
+        } catch (NullPointerException npe) {
+            // User properties not found. Using system
+        } catch (IOException e) {
+            log.warning("Loading user properties failed: " + e);
+        }
+    }
+
+    /**
+     * Take app settings from user.
+     */
+    private void initAppSettings() {
+        GameSettings localSettings = new GameSettings();
+        initSettings(localSettings);
+        settings = localSettings.toReadOnly();
+    }
+
+    private void configureApp() {
         log.debug("Configuring GameApplication");
 
         long start = System.nanoTime();
@@ -235,27 +357,6 @@ public abstract class GameApplication extends SimpleFXGLApplication {
         }
     }
 
-    @Override
-    protected final void exit() {
-        if (getSettings().isProfilingEnabled()) {
-            profiler.print();
-        }
-
-        super.exit();
-    }
-
-    //    @Override
-//    public void pause() {
-//        pushState(PauseSubState.INSTANCE);
-//        super.pause();
-//    }
-//
-//    @Override
-//    public void resume() {
-//        popState();
-//        super.resume();
-//    }
-
     /**
      * Initialize user application.
      */
@@ -306,6 +407,24 @@ public abstract class GameApplication extends SimpleFXGLApplication {
     }
 
     /**
+     * Exit the application.
+     */
+    protected final void exit() {
+        log.debug("Exiting FXGL application");
+        exitListeners.forEach(ExitListener::onExit);
+
+        if (getSettings().isProfilingEnabled()) {
+            profiler.print();
+        }
+
+        FXGL.destroy();
+        log.debug("Closing FXGL logger and exiting JavaFX");
+        log.close();
+
+        Platform.exit();
+    }
+
+    /**
      * Handler for menu events.
      */
     private MenuEventHandler menuHandler;
@@ -333,7 +452,214 @@ public abstract class GameApplication extends SimpleFXGLApplication {
         listeners.remove(listener);
     }
 
+    private List<ExitListener> exitListeners = new ArrayList<>();
+
+    /**
+     * Add listener the exit callback.
+     *
+     * @param listener the listener
+     */
+    public final void addExitListener(ExitListener listener) {
+        exitListeners.add(listener);
+    }
+
+    /**
+     * Remove previously added listener.
+     *
+     * @param listener the listener
+     */
+    public final void removeExitListener(ExitListener listener) {
+        exitListeners.remove(listener);
+    }
+
     private PlayState playState;
+
+    public final void addPlayStateListener(StateListener listener) {
+        playState.addStateListener(listener);
+    }
+
+    public final void removePlayStateListener(StateListener listener) {
+        playState.removeStateListener(listener);
+    }
+
+    /* CALLBACKS BEGIN */
+
+    // TODO: is there a reason we have to keep GameApplication extends Application,
+    // maybe hide it?
+    @Override
+    public final void init() {}
+
+    @Override
+    public final void stop() {}
+
+    /**
+     * Initialize app settings.
+     *
+     * @param settings app settings
+     */
+    protected abstract void initSettings(GameSettings settings);
+
+    /**
+     * Override to register your achievements.
+     *
+     * <pre>
+     * Example:
+     *
+     * AchievementManager am = getAchievementManager();
+     * am.registerAchievement(new Achievement("Score Master", "Score 20000 points"));
+     * </pre>
+     */
+    protected void initAchievements() {}
+
+    /**
+     * Initialize input, i.e. bind key presses, bind mouse buttons.
+     * <pre>
+     * Example:
+     *
+     * Input input = getInput();
+     * input.addAction(new UserAction("Move Left") {
+     *      protected void onAction() {
+     *          playerControl.moveLeft();
+     *      }
+     * }, KeyCode.A);
+     * </pre>
+     */
+    protected void initInput() {}
+
+    /**
+     * This is called after core services are initialized
+     * but before any game init.
+     * Called only once per application lifetime.
+     */
+    protected void preInit() {}
+
+    /**
+     * Initialize game assets, such as Texture, Sound, Music, etc.
+     */
+    protected void initAssets() {}
+
+    /**
+     * Can be overridden to provide global variables.
+     *
+     * @param vars map containing CVars (global variables)
+     */
+    protected void initGameVars(Map<String, Object> vars) {}
+
+    /**
+     * Initialize game objects.
+     */
+    protected void initGame() {}
+
+    /**
+     * Initialize collision handlers, physics properties.
+     */
+    protected void initPhysics() {}
+
+    /**
+     * Initialize UI objects.
+     */
+    protected void initUI() {}
+
+    /**
+     * Called every frame _only_ in Play state.
+     *
+     * @param tpf time per frame
+     */
+    protected void onUpdate(double tpf) {}
+
+    /**
+     * Called after main loop tick has been completed in _ANY_ state.
+     * It can be used to de-register callbacks / listeners
+     * and call various methods that otherwise might interfere
+     * with main loop.
+     *
+     * @param tpf time per frame (same as main update tpf)
+     */
+    protected void onPostUpdate(double tpf) {}
+
+    /**
+     * Called every frame in any non-Play state.
+     *
+     * @param tpf time per frame
+     */
+    protected void onPausedUpdate(double tpf) {}
+
+    /**
+     * Called when MenuEvent.SAVE occurs.
+     *
+     * @return data with required info about current state
+     * @throws UnsupportedOperationException if was not overridden
+     */
+    protected DataFile saveState() {
+        log.warning("Called saveState(), but it wasn't overridden!");
+        throw new UnsupportedOperationException("Default implementation is not available");
+    }
+
+    /**
+     * Called when MenuEvent.LOAD occurs.
+     *
+     * @param dataFile previously saved data
+     * @throws UnsupportedOperationException if was not overridden
+     */
+    protected void loadState(DataFile dataFile) {
+        log.warning("Called loadState(), but it wasn't overridden!");
+        throw new UnsupportedOperationException("Default implementation is not available");
+    }
+
+    /**
+     * Override to provide custom intro/loading/menu scenes.
+     *
+     * @return scene factory
+     */
+    @Deprecated
+    protected SceneFactory initSceneFactory() {
+        return new SceneFactory();
+    }
+
+    // TODO: choose either set or init ...
+    public void setSceneFactory(SceneFactory sceneFactory) {
+        this.sceneFactory = sceneFactory;
+    }
+
+    /* CALLBACKS END */
+
+    /* MOCKING */
+
+    /**
+     * Used by mocking.
+     *
+     * @param stage mock stage
+     */
+    void injectStage(Stage stage) {
+        primaryStage = stage;
+    }
+
+    /**
+     * Used by mocking.
+     *
+     * @param settings mock settings
+     */
+    void injectSettings(ReadOnlyGameSettings settings) {
+        this.settings = settings;
+    }
+
+    /* GETTERS */
+
+    public final AppStateMachine getStateMachine() {
+        return stateMachine;
+    }
+
+    public final SceneFactory getSceneFactory() {
+        return sceneFactory;
+    }
+
+    // TODO: we shouldn't expose primary stage anywhere within the framework
+    /**
+     * @return primary stage as set by JavaFX
+     */
+    final Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
     /**
      * @return game state
@@ -364,23 +690,107 @@ public abstract class GameApplication extends SimpleFXGLApplication {
     }
 
     /**
-     * @return game scene input
+     * @return play state input
      */
-    @Override
     public final Input getInput() {
         return playState.getInput();
     }
 
-    @Override
+    /**
+     * @return play state timer
+     */
     public final StateTimer getMasterTimer() {
         return playState.getTimer();
     }
 
-    public final void addPlayStateListener(StateListener listener) {
-        playState.addStateListener(listener);
+    /**
+     * @return read only copy of game settings
+     */
+    public final ReadOnlyGameSettings getSettings() {
+        return settings;
     }
 
-    public final void removePlayStateListener(StateListener listener) {
-        playState.removeStateListener(listener);
+    /**
+     * @return target width as set by GameSettings
+     */
+    public final int getWidth() {
+        return getSettings().getWidth();
+    }
+
+    /**
+     * @return target height as set by GameSettings
+     */
+    public final int getHeight() {
+        return getSettings().getHeight();
+    }
+
+    /**
+     * @return app bounds as set by GameSettings
+     * @apiNote equivalent to new Rectangle2D(0, 0, getWidth(), getHeight())
+     */
+    public final Rectangle2D getAppBounds() {
+        return new Rectangle2D(0, 0, getWidth(), getHeight());
+    }
+
+    /**
+     * @return current tick (frame)
+     */
+    public final long getTick() {
+        return 0;
+    }
+
+    /**
+     * @return current time since start of game in nanoseconds
+     */
+    public final long getNow() {
+        return 0;
+    }
+
+    public final EventBus getEventBus() {
+        return FXGL.getEventBus();
+    }
+
+    public final Display getDisplay() {
+        return FXGL.getDisplay();
+    }
+
+    public final AudioPlayer getAudioPlayer() {
+        return FXGL.getAudioPlayer();
+    }
+
+    public final AssetLoader getAssetLoader() {
+        return FXGL.getAssetLoader();
+    }
+
+    public final Executor getExecutor() {
+        return FXGL.getExecutor();
+    }
+
+    public final NotificationService getNotificationService() {
+        return FXGL.getNotificationService();
+    }
+
+    public final AchievementManager getAchievementManager() {
+        return FXGL.getAchievementManager();
+    }
+
+    public final QTE getQTE() {
+        return FXGL.getQTE();
+    }
+
+    public final Net getNet() {
+        return FXGL.getNet();
+    }
+
+    public final ExceptionHandler getExceptionHandler() {
+        return FXGL.getExceptionHandler();
+    }
+
+    public final UIFactory getUIFactory() {
+        return FXGL.getUIFactory();
+    }
+
+    public final QuestService getQuestService() {
+        return FXGL.getQuestManager();
     }
 }
