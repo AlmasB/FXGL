b30e7efdf20db489f705050ca2c14ff9f41a1622
AlmasB <almaslvl@gmail.com>
2017-06-22 17:01:18 +0100

    fix for wall sticking

--- fxgl-samples/src/main/java/sandbox/scifi/ScifiFactory.java
+++ fxgl-samples/src/main/java/sandbox/scifi/ScifiFactory.java
@@ -14,6 +14,9 @@ import com.almasb.fxgl.entity.*;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyDef;
+import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
@@ -57,6 +60,13 @@ public class ScifiFactory implements EntityFactory {
     @Spawns("player")
     public Entity newPlayer(SpawnData data) {
         PhysicsComponent physics = new PhysicsComponent();
+
+        physics.setFixtureDef(new FixtureDef().friction(0).density(0.25f));
+
+        BodyDef bd = new BodyDef();
+        bd.setFixedRotation(true);
+
+        physics.setBodyDef(bd);
         physics.setBodyType(BodyType.DYNAMIC);
 
         return Entities.builder()
--- fxgl-samples/src/main/java/sandbox/scifi/ScifiSample.java
+++ fxgl-samples/src/main/java/sandbox/scifi/ScifiSample.java
@@ -14,9 +14,13 @@ import com.almasb.fxgl.entity.RenderLayer;
 import com.almasb.fxgl.entity.ScrollingBackgroundView;
 import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.parser.tiled.TiledMap;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.Fixture;
 import com.almasb.fxgl.settings.GameSettings;
 import javafx.geometry.Orientation;
 import javafx.scene.input.KeyCode;
+
+import java.util.List;
 
 /**
  *
@@ -33,7 +37,7 @@ public class ScifiSample extends GameApplication {
         settings.setVersion("0.1");
         settings.setIntroEnabled(false);
         settings.setMenuEnabled(false);
-        settings.setProfilingEnabled(false);
+        settings.setProfilingEnabled(true);
         settings.setCloseConfirmation(false);
         settings.setApplicationMode(ApplicationMode.DEVELOPER);
     }
@@ -60,12 +64,22 @@ public class ScifiSample extends GameApplication {
             protected void onAction() {
                 playerControl.left();
             }
+
+            @Override
+            protected void onActionEnd() {
+                playerControl.stop();
+            }
         }, KeyCode.A);
 
         getInput().addAction(new UserAction("Right") {
             @Override
             protected void onAction() {
                 playerControl.right();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                playerControl.stop();
             }
         }, KeyCode.D);
 
@@ -86,6 +100,14 @@ public class ScifiSample extends GameApplication {
         //getGameWorld().spawn("button", 30, 340);
 
         player = (GameEntity) getGameWorld().spawn("player", 100, 100);
+//        player.getComponent(PhysicsComponent.class).setOnPhysicsInitialized(() -> {
+//            List<Fixture> fixtures = player.getComponent(PhysicsComponent.class).getBody().getFixtures();
+//            fixtures.get(0).setFriction(0);
+//            fixtures.get(0).setDensity(0.25f);
+//
+//            fixtures.get(1).setFriction(0);
+//            fixtures.get(1).setDensity(0.25f);
+//        });
         playerControl = player.getControl(PlayerControl.class);
 
         getGameScene().getViewport().setBounds(0, 0, 1920, 768);
