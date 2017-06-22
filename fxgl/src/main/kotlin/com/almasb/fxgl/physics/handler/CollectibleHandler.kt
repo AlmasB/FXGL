b5400a61dc5b05a16653127f4b5ee1ceb3c65d17
AlmasB <almaslvl@gmail.com>
2017-06-22 20:53:48 +0100

    added convenience handler for collectibles

---
+++ fxgl/src/main/kotlin/com/almasb/fxgl/physics/handler/CollectibleHandler.kt
@@ -0,0 +1,35 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package com.almasb.fxgl.physics.handler
+
+import com.almasb.fxgl.app.FXGL
+import com.almasb.fxgl.ecs.Entity
+import com.almasb.fxgl.physics.CollisionHandler
+import java.util.function.Consumer
+
+/**
+ * Generic collectible collision handler.
+ * The collectible will be removed from the world when
+ * the collector collides with it.
+ *
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+class CollectibleHandler
+@JvmOverloads constructor(collectorType: Any, collectibleType: Any,
+                          private val soundName: String = "",
+                          private val action: Consumer<Entity> = Consumer {}) : CollisionHandler(collectorType, collectibleType) {
+
+    override fun onCollisionBegin(collector: Entity, collectible: Entity) {
+        if (soundName.isNotEmpty()) {
+            FXGL.getAudioPlayer().playSound(soundName)
+        }
+
+        action.accept(collectible)
+
+        collectible.removeFromWorld()
+    }
+}
