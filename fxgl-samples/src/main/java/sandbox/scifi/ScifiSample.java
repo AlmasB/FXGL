9c2a80c0d5ed0ced90d3104879bdd4c87afc0ed2
AlmasB <almaslvl@gmail.com>
2017-06-23 14:52:05 +0100

    testing parallax bg

--- fxgl-samples/src/main/java/sandbox/scifi/ScifiSample.java
+++ fxgl-samples/src/main/java/sandbox/scifi/ScifiSample.java
@@ -9,8 +9,7 @@ package sandbox.scifi;
 import com.almasb.fxgl.app.ApplicationMode;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.ecs.Entity;
-import com.almasb.fxgl.entity.RenderLayer;
-import com.almasb.fxgl.entity.ScrollingBackgroundView;
+import com.almasb.fxgl.entity.*;
 import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.parser.tiled.TiledMap;
 import com.almasb.fxgl.physics.CollisionHandler;
@@ -19,6 +18,8 @@ import com.almasb.fxgl.physics.handler.CollectibleHandler;
 import com.almasb.fxgl.settings.GameSettings;
 import javafx.geometry.Orientation;
 import javafx.scene.input.KeyCode;
+
+import java.util.Arrays;
 
 /**
  *
@@ -83,6 +84,18 @@ public class ScifiSample extends GameApplication {
 
     private int level = 1;
 
+    private RenderLayer BG = new RenderLayer() {
+        @Override
+        public String name() {
+            return "bg";
+        }
+
+        @Override
+        public int index() {
+            return 900;
+        }
+    };
+
     private void nextLevel() {
         TiledMap map = getAssetLoader().loadJSON("mario" + level + ".json", TiledMap.class);
 
@@ -91,21 +104,51 @@ public class ScifiSample extends GameApplication {
         Entity player = getGameWorld().getEntitiesByType(ScifiType.PLAYER).get(0);
         playerControl = player.getControl(PlayerControl.class);
 
-        getGameScene().getViewport().setBounds(0, 0, 1920, 768);
+        getGameScene().getViewport().setBounds(0, 0, 3000, 768);
         getGameScene().getViewport().bindToEntity(player, 500, 0);
 
-        getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_wrap.png", 1280, 768),
-                Orientation.HORIZONTAL, new RenderLayer() {
-            @Override
-            public String name() {
-                return "Scroll";
-            }
+//        EntityView bg = new EntityView(BG);
+//
+//        bg.addNode(getAssetLoader().loadTexture("bg_10.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_9.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_8.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_7.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_6.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_5.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_4.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_3.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_2.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_1.png", 1280, 768));
+//        bg.addNode(getAssetLoader().loadTexture("bg_0.png", 1280, 768));
+//
+//        getGameScene().addGameView(bg);
 
-            @Override
-            public int index() {
-                return 990;
-            }
-        }));
+//        getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_10.png", 1280, 768), Orientation.HORIZONTAL, 1.0, BG));
+//        //getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_9.png", 1280, 768), Orientation.HORIZONTAL, 1.0, BG));
+//        //getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_8.png", 1280, 768), Orientation.HORIZONTAL, 1.0, BG));
+//        getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_7.png", 1280, 768), Orientation.HORIZONTAL, 0.3, BG));
+//        //getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_6.png", 1280, 768), Orientation.HORIZONTAL, 1.0, BG));
+//        getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_5.png", 1280, 768), Orientation.HORIZONTAL, 0.5, BG));
+//        //getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_4.png", 1280, 768), Orientation.HORIZONTAL, 1.0, BG));
+//        getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_3.png", 1280, 768), Orientation.HORIZONTAL, 0.7, BG));
+//        //getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_2.png", 1280, 768), Orientation.HORIZONTAL, 1.0, BG));
+//        getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_1.png", 1280, 768), Orientation.HORIZONTAL, 0.8, BG));
+//        getGameScene().addGameView(new ScrollingBackgroundView(getAssetLoader().loadTexture("bg_0.png", 1280, 768), Orientation.HORIZONTAL, 0.85, BG));
+
+        // assets from https://raventale.itch.io/parallax-background
+        getGameScene().addGameView(new ParallaxBackgroundView(Arrays.asList(
+                new ParallaxTexture(getAssetLoader().loadTexture("bg_10.png", 1280, 768), 1.0),
+                new ParallaxTexture(getAssetLoader().loadTexture("bg_9.png", 1280, 768), 0.05),
+                new ParallaxTexture(getAssetLoader().loadTexture("bg_8.png", 1280, 768), 0.1),
+                new ParallaxTexture(getAssetLoader().loadTexture("bg_7.png", 1280, 768), 0.3),
+                new ParallaxTexture(getAssetLoader().loadTexture("bg_6.png", 1280, 768), 0.45),
+                //new ParallaxTexture(getAssetLoader().loadTexture("bg_5.png", 1280, 768), 0.45),
+                //new ParallaxTexture(getAssetLoader().loadTexture("bg_4.png", 1280, 768), 0.6),
+                new ParallaxTexture(getAssetLoader().loadTexture("bg_3.png", 1280, 768), 0.5),
+                new ParallaxTexture(getAssetLoader().loadTexture("bg_2.png", 1280, 768), 0.7),
+                new ParallaxTexture(getAssetLoader().loadTexture("bg_1.png", 1280, 768), 0.8),
+                new ParallaxTexture(getAssetLoader().loadTexture("bg_0.png", 1280, 768), 0.9)
+        ), Orientation.HORIZONTAL, BG));
 
         level++;
         if (level == 3)
