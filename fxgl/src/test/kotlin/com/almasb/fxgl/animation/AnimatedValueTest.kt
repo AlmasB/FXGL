61817754898ca5278ad795efdddb39ca135a1d93
AlmasB <almaslvl@gmail.com>
2017-07-20 21:16:31 +0500

    added point2d and double animated value test

---
+++ fxgl/src/test/kotlin/com/almasb/fxgl/animation/AnimatedValueTest.kt
@@ -0,0 +1,36 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package com.almasb.fxgl.animation
+
+import javafx.geometry.Point2D
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+class AnimatedValueTest {
+
+    @Test
+    fun `Point2D`() {
+        val anim = AnimatedPoint2D(Point2D(0.0, 0.0), Point2D(100.0, 100.0))
+
+        assertThat(anim.getValue(0.0), `is`(Point2D(0.0, 0.0)))
+        assertThat(anim.getValue(1.0), `is`(Point2D(100.0, 100.0)))
+        assertThat(anim.getValue(0.5), `is`(Point2D(50.0, 50.0)))
+    }
+
+    @Test
+    fun `Double`() {
+        val anim = AnimatedValue<Double>(100.0, 200.0)
+
+        assertThat(anim.getValue(0.0), `is`(100.0))
+        assertThat(anim.getValue(1.0), `is`(200.0))
+        assertThat(anim.getValue(0.5), `is`(150.0))
+    }
+}
