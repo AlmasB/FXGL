5f526b64f3944329f7e7041184106702385ff6e8
AlmasB <almaslvl@gmail.com>
2017-08-10 22:27:27 +0500

    improved patrol task

--- fxgl-samples/src/main/java/s08ai/PatrolTask.java
+++ fxgl-samples/src/main/java/s08ai/PatrolTask.java
@@ -7,11 +7,21 @@
 package s08ai;
 
 import com.almasb.fxgl.ai.Action;
+import com.almasb.fxgl.core.math.FXGLMath;
+import javafx.geometry.Point2D;
 
 /**
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
 public class PatrolTask extends Action {
+
+    private static final Point2D[] POINTS = new Point2D[] {
+            new Point2D(300, 300),
+            new Point2D(500, 500),
+            new Point2D(450, 250)
+    };
+
+    private Point2D selectedPoint = POINTS[0];
 
     public PatrolTask() {
         super("Patrol");
@@ -19,6 +29,10 @@ public class PatrolTask extends Action {
 
     @Override
     public void action() {
+        getObject().translateTowards(selectedPoint, 1);
 
+        if (getObject().getPosition().distance(selectedPoint) < 5) {
+            selectedPoint = POINTS[FXGLMath.random(0, 2)];
+        }
     }
 }
