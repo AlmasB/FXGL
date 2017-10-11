fb5dd979e0837d3e63e71a1acd0157de64f682a7
AlmasB <almaslvl@gmail.com>
2017-10-11 18:51:24 +0100

    cleanup

---
+++ fxgl-samples/src/main/java/s03entities/ExpireSample.java
@@ -0,0 +1,50 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package s03entities;
+
+import com.almasb.fxgl.app.ApplicationMode;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.control.ExpireCleanControl;
+import com.almasb.fxgl.settings.GameSettings;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
+/**
+ *
+ *
+ * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+ */
+public class ExpireSample extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("ExpireSample");
+        settings.setVersion("0.1");
+        settings.setFullScreen(false);
+        settings.setIntroEnabled(false);
+        settings.setMenuEnabled(false);
+        settings.setProfilingEnabled(false);
+        settings.setCloseConfirmation(false);
+        settings.setApplicationMode(ApplicationMode.DEVELOPER);
+    }
+
+    @Override
+    protected void initGame() {
+        Entities.builder()
+                .at(100, 100)
+                .viewFromNode(new Rectangle(40, 40))
+                .with(new ExpireCleanControl(Duration.seconds(2)).animateOpacity())
+                .buildAndAttach(getGameWorld());
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
