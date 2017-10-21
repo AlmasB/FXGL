48012a9ed200c474f2ac387c4fdef7d2c288f8b1
AlmasB <almaslvl@gmail.com>
2017-10-21 14:54:51 +0100

    added a dialogs sample showing dialog types, closes #400

--- fxgl-samples/src/main/java/s05uimenus/DialogsSample.java
+++ fxgl-samples/src/main/java/s05uimenus/DialogsSample.java
@@ -8,14 +8,19 @@ package s05uimenus;
 
 import com.almasb.fxgl.app.ApplicationMode;
 import com.almasb.fxgl.app.GameApplication;
-import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.settings.GameSettings;
-import com.almasb.fxgl.util.InputPredicates;
-import javafx.scene.input.KeyCode;
+import com.almasb.fxgl.texture.Texture;
+import javafx.collections.FXCollections;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
- * Shows an example of FXGL dialogs.
- * Press F to open a dialog.
+ * Shows all FXGL dialogs.
  *
  * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
  */
@@ -30,20 +35,50 @@ public class DialogsSample extends GameApplication {
         settings.setFullScreen(false);
         settings.setIntroEnabled(false);
         settings.setMenuEnabled(false);
-        settings.setProfilingEnabled(true);
+        settings.setProfilingEnabled(false);
+        settings.setCloseConfirmation(false);
         settings.setApplicationMode(ApplicationMode.DEVELOPER);
     }
 
     @Override
-    protected void initInput() {
-        getInput().addAction(new UserAction("Dialog") {
-            @Override
-            protected void onActionBegin() {
-                // 1. get display service and use one of the show* methods
-                // this shows a dialog that only takes alphanumerical characters as input
-                getDisplay().showInputBox("Simple Dialog", InputPredicates.ALPHANUM, System.out::println);
+    protected void initUI() {
+        Map<String, Runnable> dialogs = new LinkedHashMap<>();
+        dialogs.put("Message", () -> getDisplay().showMessageBox("This is a simple message box"));
+        dialogs.put("Error", () -> getDisplay().showErrorBox("This is a scary error box!", () -> {}));
+        dialogs.put("Confirmation", () -> getDisplay().showConfirmationBox("This is a confirmation box. Agree?", answer -> System.out.println("You pressed yes? " + answer)));
+        dialogs.put("Input", () -> getDisplay().showInputBox("This is an input box. You can type stuff...", answer -> System.out.println("You typed: "+ answer)));
+        dialogs.put("Custom", () -> {
+            Texture brick = getAssetLoader().loadTexture("brick.png");
+
+            Button btnClose = getUIFactory().newButton("Press me to close");
+            btnClose.setPrefWidth(300);
+
+            getDisplay().showBox("This is a customizable box", brick, btnClose);
+        });
+
+        ChoiceBox<String> cbDialogs = getUIFactory().newChoiceBox(FXCollections.observableArrayList(dialogs.keySet()));
+
+        cbDialogs.getSelectionModel().selectFirst();
+
+        Button btn = getUIFactory().newButton("Open");
+        btn.setOnAction(e -> {
+            String dialogType = cbDialogs.getSelectionModel().getSelectedItem();
+            if (dialogs.containsKey(dialogType)) {
+                dialogs.get(dialogType).run();
+            } else {
+                System.out.println("Unknown dialog type");
             }
-        }, KeyCode.F);
+        });
+
+        VBox vbox = new VBox(10);
+        vbox.setTranslateX(600);
+        vbox.getChildren().addAll(
+                new Text("Dialog Types"),
+                cbDialogs,
+                btn
+        );
+
+        getGameScene().addUINode(vbox);
     }
 
     public static void main(String[] args) {
