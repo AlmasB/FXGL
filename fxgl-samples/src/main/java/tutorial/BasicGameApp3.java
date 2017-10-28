ce7a6bfe3c5342232b5fda79a89fed72921619f1
AlmasB <almaslvl@gmail.com>
2017-10-28 15:04:05 +0100

    added tutorial 3

---
+++ fxgl-samples/src/main/java/tutorial/BasicGameApp3.java
@@ -0,0 +1,147 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package tutorial;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.component.CollidableComponent;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.physics.CollisionHandler;
+import com.almasb.fxgl.settings.GameSettings;
+import com.almasb.fxgl.texture.Texture;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+
+import java.util.Map;
+
+/**
+ * This is a basic FXGL game application tutorial.
+ *
+ * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+ */
+public class BasicGameApp3 extends GameApplication {
+
+    public enum EntityType {
+        PLAYER, COIN
+    }
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(600);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App3");
+        settings.setVersion("0.1");
+        settings.setProfilingEnabled(false);  // turn off fps
+        settings.setCloseConfirmation(false); // turn off exit dialog
+        settings.setIntroEnabled(false);      // turn off intro
+        settings.setMenuEnabled(false);       // turn off menus
+    }
+
+    @Override
+    protected void initInput() {
+        Input input = getInput(); // get input service
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player.translateX(5); // move right 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                player.translateX(-5); // move left 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player.translateY(-5); // move up 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                player.translateY(5); // move down 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.S);
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
+
+    private Entity player;
+
+    @Override
+    protected void initGame() {
+        player = Entities.builder()
+                .type(EntityType.PLAYER)
+                .at(300, 300)
+                .viewFromTextureWithBBox("brick.png")
+                .with(new CollidableComponent(true))
+                .buildAndAttach(getGameWorld());
+
+        Entities.builder()
+                .type(EntityType.COIN)
+                .at(500, 200)
+                .viewFromNodeWithBBox(new Circle(15, Color.YELLOW))
+                .with(new CollidableComponent(true))
+                .buildAndAttach(getGameWorld());
+
+        getGameState().<Integer>addListener("pixelsMoved", (prev, now) -> {
+            if (now % 100 == 0) {
+                getAudioPlayer().playSound("drop.wav");
+            }
+        });
+    }
+
+    @Override
+    protected void initPhysics() {
+        getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityType.PLAYER, EntityType.COIN) {
+
+            // order of types is the same as passed into the constructor
+            @Override
+            protected void onCollisionBegin(Entity player, Entity coin) {
+                coin.removeFromWorld();
+            }
+        });
+    }
+
+    @Override
+    protected void initUI() {
+        Text textPixels = new Text();
+        textPixels.setTranslateX(50); // x = 50
+        textPixels.setTranslateY(100); // y = 100
+
+        textPixels.textProperty().bind(getGameState().intProperty("pixelsMoved").asString());
+
+        getGameScene().addUINode(textPixels); // add to the scene graph
+
+        Texture brickTexture = getAssetLoader().loadTexture("brick.png");
+        brickTexture.setTranslateX(50);
+        brickTexture.setTranslateY(450);
+
+        getGameScene().addUINode(brickTexture);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
