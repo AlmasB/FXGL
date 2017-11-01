07e8ac0d7787caf4209fbc7a5b58c9d7b3c94b3d
AlmasB <almaslvl@gmail.com>
2017-11-01 22:58:00 +0000

    more blend functions

--- fxgl-samples/src/main/java/s10miscellaneous/ColoringSample.java
+++ fxgl-samples/src/main/java/s10miscellaneous/ColoringSample.java
@@ -14,7 +14,12 @@ import javafx.scene.effect.BlendMode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 import static com.almasb.fxgl.app.DSLKt.texture;
 
@@ -39,9 +44,9 @@ public class ColoringSample extends GameApplication {
 
     @Override
     protected void initUI() {
-        //getGameScene().setBackgroundColor(Color.BLACK);
+        getGameScene().setBackgroundColor(Color.color(0.75, 0.75, 0.75));
 
-        Texture original = texture("bird.png");
+        Texture original = texture("player2.png");
 
 //        long start = System.nanoTime();
 //
@@ -55,26 +60,66 @@ public class ColoringSample extends GameApplication {
 //
 //        System.out.println(System.nanoTime() - start);
 
+        LinearGradient gradient = new LinearGradient(0.5, 0, 0.5, 1, true, CycleMethod.NO_CYCLE,
+                new Stop(0, Color.RED),
+                new Stop(1, Color.YELLOW));
 
         HBox hbox = new HBox(20,
                 makeBox("Original", original),
-                makeBox("Alpha: black", original.transparentColor(Color.BLACK)),
                 makeBox("Grayscale", original.toGrayscale()),
                 makeBox("Darker", original.darker()),
-                makeBox("Brighter", original.brighter())
+                makeBox("Brighter", original.brighter()),
+                makeBox("Invert", original.invert()),
+                makeBox("Saturate", original.saturate().saturate().saturate())
         );
 
         HBox hbox2 = new HBox(20,
-                makeBox("Blend: red", original.blend(new Rectangle(100, 100, Color.RED), BlendMode.ADD)),
-                makeBox("Blend: red", original.blend(new Rectangle(200, 100, Color.BLUE), BlendMode.RED)),
-                //makeBox("Colorize: red", original.toColor(Color.RED)),
-                makeBox("Multiply: red", original.multiplyColor(Color.RED))
+                makeBox("Soft: self", original.blend(original.copy(), BlendMode.SOFT_LIGHT)),
+                makeBox("Add: red", original.blend(new Rectangle(200, 100, Color.RED), BlendMode.ADD)),
+                makeBox("Red on blue", original.blend(new Rectangle(200, 100, Color.BLUE), BlendMode.RED)),
+                makeBox("Color: red", original.toColor(Color.RED)),
+                makeBox("Mult: red", original.multiplyColor(Color.RED)),
+                makeBox("Screen: RY", original.blend(new Rectangle(200, 100, gradient), BlendMode.SCREEN))
         );
 
-        hbox.setTranslateY(100);
-        hbox2.setTranslateY(300);
 
-        getGameScene().addUINodes(hbox, hbox2);
+
+        Texture brick1 = texture("brick.png");
+        brick1.setScaleX(1.5);
+        brick1.setScaleY(1.5);
+
+        Texture brick2 = texture("brick.png");
+        brick2.setScaleX(1.5);
+        brick2.setScaleY(1.5);
+
+        Texture brick3 = texture("brick.png");
+        brick3.setScaleX(1.5);
+        brick3.setScaleY(1.5);
+
+        Texture brick4 = texture("brick.png");
+        brick4.setScaleX(1.5);
+        brick4.setScaleY(1.5);
+
+        Texture ship1 = original.copy();
+        ship1.setScaleX(0.7);
+        ship1.setScaleY(0.7);
+
+        HBox hbox3 = new HBox(20,
+                makeBox("Texture 2", texture("brick.png")),
+                makeBox("Diff", original.blend(brick1, BlendMode.DIFFERENCE)),
+                makeBox("Diff^-1", brick3.blend(ship1, BlendMode.DIFFERENCE)),
+                makeBox("Overlay", original.blend(brick2, BlendMode.OVERLAY)),
+                makeBox("Exclusion", original.blend(brick4, BlendMode.EXCLUSION))
+        );
+
+        hbox.setTranslateY(50);
+        hbox2.setTranslateY(250);
+        hbox3.setTranslateY(450);
+
+        Text t = getUIFactory().newText("Texture v Texture Blend", Color.WHITE, 24);
+        getUIFactory().centerText(t, getWidth() / 2, getHeight() - 150);
+
+        getGameScene().addUINodes(hbox, hbox2, hbox3, t);
     }
 
     private VBox makeBox(String header, Texture texture) {
