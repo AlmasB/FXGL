e4313751cf42ff542894dfab406d3402009bb8d1
AlmasB <almaslvl@gmail.com>
2017-11-05 13:19:15 +0000

    added robot sample

---
+++ fxgl-samples/src/main/java/sandbox/robots/MarioApp.java
@@ -0,0 +1,164 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package sandbox.robots;
+
+import com.almasb.fxgl.app.ApplicationMode;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.RenderLayer;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.physics.CollisionHandler;
+import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.physics.handler.CollectibleHandler;
+import com.almasb.fxgl.settings.GameSettings;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+
+
+/**
+ *
+ *
+ * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+ */
+public class MarioApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(1280);
+        settings.setHeight(768);
+        settings.setTitle("MarioApp");
+        settings.setVersion("0.2");
+        settings.setIntroEnabled(false);
+        settings.setMenuEnabled(false);
+        settings.setProfilingEnabled(false);
+        settings.setCloseConfirmation(false);
+        settings.setSingleStep(false);
+        settings.setApplicationMode(ApplicationMode.DEVELOPER);
+    }
+
+    private PlayerControl playerControl;
+
+    @Override
+    protected void initInput() {
+        getInput().addAction(new UserAction("Left") {
+            @Override
+            protected void onAction() {
+                playerControl.left();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                playerControl.stop();
+            }
+        }, KeyCode.A);
+
+        getInput().addAction(new UserAction("Right") {
+            @Override
+            protected void onAction() {
+                playerControl.right();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                playerControl.stop();
+            }
+        }, KeyCode.D);
+
+        getInput().addAction(new UserAction("Jump") {
+            @Override
+            protected void onActionBegin() {
+                playerControl.jump();
+            }
+        }, KeyCode.W);
+
+        getInput().addAction(new UserAction("Enter") {
+            @Override
+            protected void onActionBegin() {
+                stepLoop();
+            }
+        }, KeyCode.L);
+    }
+
+    @Override
+    protected void preInit() {
+        getGameScene().setBackgroundColor(Color.rgb(92, 148, 252));
+    }
+
+    @Override
+    protected void initGame() {
+        nextLevel();
+    }
+
+    private int level = 1;
+
+    private RenderLayer BG = new RenderLayer() {
+        @Override
+        public String name() {
+            return "bg";
+        }
+
+        @Override
+        public int index() {
+            return 900;
+        }
+    };
+
+    private void nextLevel() {
+
+        getGameWorld().setLevelFromMap("mario" + 0 + ".json");
+
+        getGameWorld().spawn("player", 10, 24 * 70 - 768);
+
+        Entity player = getGameWorld().getEntitiesByType(MarioType.PLAYER).get(0);
+        playerControl = player.getControl(PlayerControl.class);
+
+        getGameScene().getViewport().setBounds(0, 0, 64*70, 24 * 70 - 70);
+        getGameScene().getViewport().bindToEntity(player, 500, 0);
+
+        getGameWorld().spawn("robot", 1500, 24 * 70 - 1300);
+
+        level++;
+        if (level == 4)
+            level = 1;
+    }
+
+    @Override
+    protected void initPhysics() {
+        getPhysicsWorld().addCollisionHandler(new CollisionHandler(MarioType.PLAYER, MarioType.PLATFORM) {
+            @Override
+            protected void onHitBoxTrigger(Entity a, Entity b, HitBox boxA, HitBox boxB) {
+                if (boxA.getName().equals("lower")) {
+                    playerControl.canJump = true;
+                }
+            }
+        });
+
+        getPhysicsWorld().addCollisionHandler(new CollisionHandler(MarioType.PLAYER, MarioType.PORTAL) {
+            @Override
+            protected void onCollisionBegin(Entity a, Entity b) {
+                nextLevel = true;
+            }
+        });
+
+        getPhysicsWorld().addCollisionHandler(new CollectibleHandler(MarioType.PLAYER, MarioType.COIN, "drop.wav"));
+    }
+
+    private boolean nextLevel = false;
+
+    @Override
+    protected void onPostUpdate(double tpf) {
+        if (nextLevel) {
+            nextLevel = false;
+            nextLevel();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ fxgl-samples/src/main/java/sandbox/robots/MarioFactory.java
@@ -0,0 +1,111 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package sandbox.robots;
+
+import com.almasb.fxgl.annotation.SetEntityFactory;
+import com.almasb.fxgl.annotation.Spawns;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.component.CollidableComponent;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyDef;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
+import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
+import javafx.geometry.Point2D;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+@SetEntityFactory
+public class MarioFactory implements EntityFactory {
+
+    @Spawns("platform")
+    public Entity newPlatform(SpawnData data) {
+        return Entities.builder()
+                .from(data)
+                .type(MarioType.PLATFORM)
+                .bbox(new HitBox("main", BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
+                .with(new PhysicsComponent(), new CollidableComponent(true))
+                .build();
+    }
+
+    @Spawns("ghost_platform")
+    public Entity newGhostPlatform(SpawnData data) {
+        return Entities.builder()
+                .from(data)
+                .type(MarioType.PLATFORM)
+                .bbox(new HitBox("main", BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
+                .with(new PhysicsComponent(), new CollidableComponent(true))
+                .build();
+    }
+
+    @Spawns("coin")
+    public Entity newCoin(SpawnData data) {
+        return Entities.builder()
+                .from(data)
+                .type(MarioType.COIN)
+                .viewFromTextureWithBBox("coin.png")
+                .with(new CollidableComponent(true))
+                .build();
+    }
+
+    @Spawns("player")
+    public Entity newPlayer(SpawnData data) {
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setFixtureDef(new FixtureDef().friction(0).density(0.25f));
+
+        BodyDef bd = new BodyDef();
+        bd.setFixedRotation(true);
+
+        physics.setBodyDef(bd);
+        physics.setBodyType(BodyType.DYNAMIC);
+
+        return Entities.builder()
+                .from(data)
+                .type(MarioType.PLAYER)
+                .bbox(new HitBox("main", BoundingShape.circle(15)))
+                .bbox(new HitBox("lower", new Point2D(15 - 5, 30), BoundingShape.box(10, 10)))
+                .with(physics, new CollidableComponent(true))
+                .with(new PlayerControl())
+                .build();
+    }
+
+    @Spawns("robot")
+    public Entity newRobot(SpawnData data) {
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setFixtureDef(new FixtureDef().friction(0).density(0.25f));
+
+        BodyDef bd = new BodyDef();
+        bd.setFixedRotation(true);
+
+        physics.setBodyDef(bd);
+        physics.setBodyType(BodyType.DYNAMIC);
+
+        return Entities.builder()
+                .from(data)
+                .type(MarioType.ROBOT)
+                .bbox(new HitBox("main", new Point2D(275 / 2 - 105/2, 275 / 2 - 210/2), BoundingShape.box(105, 210)))
+                .bbox(new HitBox("lower", new Point2D(105 / 2 - 5, 125*2), BoundingShape.box(10, 10)))
+                .with(physics, new CollidableComponent(true))
+                .with(new RobotControl())
+                .build();
+    }
+
+    @Spawns("portal")
+    public Entity newPortal(SpawnData data) {
+        return Entities.builder()
+                .type(MarioType.PORTAL)
+                .from(data)
+                .viewFromTextureWithBBox("finish.png")
+                .with(new CollidableComponent(true))
+                .build();
+    }
+}
