170f42a779fec4b36f3a640901309146002f78b3
AlmasB <almaslvl@gmail.com>
2018-01-08 20:33:19 +0000

    clear game state listeners on reset and on remove, added missing tests, closes #435

--- fxgl/src/test/kotlin/com/almasb/fxgl/gameplay/GameStateTest.kt
+++ fxgl/src/test/kotlin/com/almasb/fxgl/gameplay/GameStateTest.kt
@@ -12,8 +12,7 @@ import org.hamcrest.CoreMatchers.isA
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.collection.IsMapContaining
 import org.hamcrest.collection.IsMapContaining.hasEntry
-import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -67,10 +66,24 @@ class GameStateTest {
 
     @Test
     fun `Test set`() {
-        gameState.put("testInt", 100)
+        gameState.put("testBoolean", true)
+        gameState.put("testInt", 5)
+        gameState.put("testDouble", 10.5)
+        gameState.put("testString", "StringData")
+        gameState.put("testObject", Dummy("ObjectData"))
 
-        gameState.setValue("testInt", 200)
-        assertThat(gameState.getInt("testInt"), `is`(200))
+        gameState.setValue("testBoolean", false)
+        gameState.setValue("testInt", 50)
+        gameState.setValue("testDouble", 100.5)
+        gameState.setValue("testString", "StringDataNew")
+        gameState.setValue("testObject", Dummy("ObjectDataNew"))
+
+        assertFalse(gameState.getBoolean("testBoolean"))
+        assertThat(gameState.getInt("testInt"), `is`(50))
+        assertThat(gameState.getDouble("testDouble"), `is`(100.5))
+        assertThat(gameState.getString("testString"), `is`("StringDataNew"))
+        assertTrue(gameState.getType("testObject") == (SimpleObjectProperty::class.java))
+        assertThat(gameState.getObject<Dummy>("testObject").data, `is`("ObjectDataNew"))
     }
 
     @Test
@@ -112,14 +125,21 @@ class GameStateTest {
 
         var count = 0
 
-        gameState.addListener<Int>("testInt", object : PropertyChangeListener<Int> {
+        val listener = object : PropertyChangeListener<Int> {
             override fun onChange(prev: Int, now: Int) {
                 assertThat(prev, `is`(10))
                 count += now
             }
-        })
+        }
+
+        gameState.addListener("testInt", listener)
 
         gameState.setValue("testInt", 25)
+        assertThat(count, `is`(25))
+
+        gameState.removeListener("testInt", listener)
+
+        gameState.setValue("testInt", 1000)
         assertThat(count, `is`(25))
     }
 
@@ -128,6 +148,24 @@ class GameStateTest {
         gameState.gameDifficultyProperty().value = GameDifficulty.NIGHTMARE
 
         assertThat(gameState.getGameDifficulty(), `is`(GameDifficulty.NIGHTMARE))
+    }
+
+    @Test
+    fun `Clear`() {
+        gameState.put("testInt", 10)
+
+        val listener = object : PropertyChangeListener<Int> {
+            override fun onChange(prev: Int, now: Int) {
+            }
+        }
+
+        gameState.addListener("testInt", listener)
+
+        gameState.clear()
+
+        assertThrows(IllegalArgumentException::class.java, {
+            gameState.getInt("testInt")
+        })
     }
 
     private class Dummy(var data: String) {}
