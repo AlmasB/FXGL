26ed6c02ff6a309c89b5d07d4e1aec9d8e6b75d4
AlmasB <almaslvl@gmail.com>
2018-02-01 12:49:55 +0000

    ignore any unknown Tiled tags, support infinite tag, closes #461

--- fxgl/src/main/kotlin/com/almasb/fxgl/parser/tiled/TiledMap.kt
+++ fxgl/src/main/kotlin/com/almasb/fxgl/parser/tiled/TiledMap.kt
@@ -6,13 +6,16 @@
 
 package com.almasb.fxgl.parser.tiled
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
 /**
  * JSON map format from the Tiled map editor.
  * Tiled version: 1.0.1
- * Specification: https://github.com/bjorn/tiled/wiki/JSON-Map-Format
+ * Specification: http://docs.mapeditor.org/en/latest/reference/json-map-format/
  *
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 class TiledMap(var width: Int = 0,
                var height: Int = 0,
                var tilewidth: Int = 0,
@@ -25,7 +28,8 @@ class TiledMap(var width: Int = 0,
                var nextobjectid: Int = 0,
                var version: Int = 0,
                var tiledversion: String = "",
-               var type: String = "") {
+               var type: String = "",
+               var infinite: Boolean = false) {
 
     fun getLayerByName(name: String) = layers.filter { it.name == name }.firstOrNull()
             ?: throw IllegalArgumentException("Layer with name=$name not found")
