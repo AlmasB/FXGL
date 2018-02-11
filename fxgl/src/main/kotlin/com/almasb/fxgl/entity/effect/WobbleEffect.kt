36379f0ce60d0e83e5d8445d57c8d6a34354d4cd
AlmasB <almaslvl@gmail.com>
2018-02-11 12:49:00 +0000

    updated wobble effect with params

--- fxgl/src/main/kotlin/com/almasb/fxgl/entity/effect/WobbleEffect.kt
+++ fxgl/src/main/kotlin/com/almasb/fxgl/entity/effect/WobbleEffect.kt
@@ -6,62 +6,97 @@
 
 package com.almasb.fxgl.entity.effect
 
-import com.almasb.fxgl.app.FXGL
 import com.almasb.fxgl.core.math.FXGLMath
 import com.almasb.fxgl.entity.Effect
 import com.almasb.fxgl.entity.Entity
 import com.almasb.fxgl.entity.view.EntityView
 import com.almasb.fxgl.texture.Texture
+import javafx.geometry.Orientation
 import javafx.geometry.Rectangle2D
-import javafx.scene.Node
+import javafx.scene.SnapshotParameters
+import javafx.scene.paint.Color
 import javafx.util.Duration
-import java.util.ArrayList
+import java.util.*
 
 /**
- *
+ * Note: this effect destroys the original entity view (along with its reference) and only records
+ * the view snapshot.
+ * So the effect can't be applied to anything animated.
  *
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
-class WobbleEffect(duration: Duration) : Effect(duration) {
+class WobbleEffect
+@JvmOverloads constructor(
+        duration: Duration,
+        val radius: Int = 2,
+        val numChunks: Int = 5,
+        val orientation: Orientation = Orientation.HORIZONTAL
+) : Effect(duration) {
 
     private val quads = ArrayList<Texture>()
-    private lateinit var originalView: Node
+    private var originalTexture: Texture? = null
+
+    private var tick = 0
 
     override fun onStart(entity: Entity) {
-        //originalView = entity.view
+        tick = 0
 
-        val originalTexture = Texture(entity.view.snapshot(null, null))
-        originalView = originalTexture
+        if (originalTexture == null) {
+            val params = SnapshotParameters()
+            params.fill = Color.TRANSPARENT
+
+            originalTexture = Texture(entity.view.snapshot(params, null))
+        }
 
         // TODO: need to copy render layer
         val newView = EntityView()
 
-        val numChunks = 8
+        val chunkSize = (if (orientation == Orientation.HORIZONTAL)
+            originalTexture!!.image.height / numChunks
+        else
+            originalTexture!!.image.width / numChunks).toInt().toDouble()
 
-        val chunkSize = originalTexture.image.width / numChunks
 
-        println(chunkSize)
 
         for (i in 0 until numChunks) {
-            val quad = originalTexture.subTexture(Rectangle2D(0.0, i * chunkSize, originalTexture.image.width, chunkSize))
-            quad.translateY = i * chunkSize
 
-            quads.add(quad)
+            if (orientation == Orientation.HORIZONTAL) {
+                val quad = originalTexture!!.subTexture(Rectangle2D(0.0, i * chunkSize, originalTexture!!.image.width, chunkSize))
 
-            newView.addNode(quad)
+
+                quad.translateY = i * chunkSize
+
+                quads.add(quad)
+
+                newView.addNode(quad)
+            } else {
+                val quad = originalTexture!!.subTexture(Rectangle2D(i * chunkSize, 0.0, chunkSize, originalTexture!!.image.height))
+
+
+                quad.translateX = i * chunkSize
+
+                quads.add(quad)
+
+                newView.addNode(quad)
+            }
         }
 
         entity.setView(newView)
     }
 
     override fun onUpdate(entity: Entity, tpf: Double) {
-        for (i in quads.indices) {
-            val t = quads.get(i)
-            t.translateX = FXGLMath.sin((FXGL.getApp().tick + i / 0.5f).toDouble()) * 1.5
+        tick += 1
+
+        quads.forEachIndexed { index, quad ->
+            if (orientation == Orientation.HORIZONTAL) {
+                quad.translateX = FXGLMath.sin((tick + index / 0.5)) * radius
+            } else {
+                quad.translateY = FXGLMath.sin((tick + index / 0.5)) * radius
+            }
         }
     }
 
     override fun onEnd(entity: Entity) {
-        entity.setView(originalView)
+        entity.setView(originalTexture)
     }
 }
