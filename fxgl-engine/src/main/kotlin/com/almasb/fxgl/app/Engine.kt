f4649e0dc44d4c92d2e0f05b06428696f585cbcb
Almas Baimagambetov <almaslvl@gmail.com>
2018-10-23 19:48:34 +0100

    experiments

--- fxgl-engine/src/main/kotlin/com/almasb/fxgl/app/Engine.kt
+++ fxgl-engine/src/main/kotlin/com/almasb/fxgl/app/Engine.kt
@@ -16,6 +16,7 @@ import com.almasb.fxgl.saving.SaveEvent
 import com.almasb.fxgl.scene.FXGLScene
 import com.almasb.fxgl.settings.ReadOnlyGameSettings
 import com.almasb.fxgl.ui.ErrorDialog
+import com.almasb.fxgl.ui.FXGLUIConfig
 import com.gluonhq.charm.down.Platform
 import com.gluonhq.charm.down.Services
 import com.gluonhq.charm.down.plugins.LifecycleEvent
@@ -115,6 +116,8 @@ internal class Engine(
         Async.start {
             IOTask.setDefaultExecutor(executor)
             IOTask.setDefaultFailAction(settings.exceptionHandler)
+
+            FXGLUIConfig.setUIFactory(settings.uiFactory)
 
             isFirstRun = !FS.exists("system/")
 
---
+++ fxgl-ui/src/test/kotlin/com/almasb/fxgl/ui/MockTest.kt
@@ -0,0 +1,15 @@
+package com.almasb.fxgl.ui
+
+import javafx.collections.FXCollections
+import org.junit.jupiter.api.Test
+
+/**
+ *
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+class MockTest {
+
+    @Test
+    fun `T`() {
+    }
+}
