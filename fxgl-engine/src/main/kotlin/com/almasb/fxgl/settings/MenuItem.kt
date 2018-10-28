9bc0ed20a5673545c5ab135056aebd2062ed3f8a
Almas Baimagambetov <almaslvl@gmail.com>
2018-10-28 13:52:04 +0000

    java -> kotlin

--- fxgl-engine/src/main/kotlin/com/almasb/fxgl/settings/MenuItem.kt
+++ fxgl-engine/src/main/kotlin/com/almasb/fxgl/settings/MenuItem.kt
@@ -1,15 +1,9 @@
-/*
- * FXGL - JavaFX Game Library. The MIT License (MIT).
- * Copyright (c) AlmasB (almaslvl@gmail.com).
- * See LICENSE for details.
- */
-
-package com.almasb.fxgl.settings;
+package com.almasb.fxgl.settings
 
 /**
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
-public enum MenuItem {
+enum class MenuItem {
 
     /**
      * Enables CONTINUE, SAVE, LOAD.
