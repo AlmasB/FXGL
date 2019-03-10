93a5a342a00be209306143c20051a3e12969a538
Almas Baimagambetov <almaslvl@gmail.com>
2019-03-10 19:06:12 +0000

    restored transform comp test, removed unused files

--- fxgl-entity/src/main/kotlin/com/almasb/fxgl/entity/components/TransformComponent.kt
+++ fxgl-entity/src/main/kotlin/com/almasb/fxgl/entity/components/TransformComponent.kt
@@ -11,9 +11,6 @@ import javafx.beans.property.SimpleDoubleProperty
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.geometry.Point2D
 
-
-//data class Transform(val x: Double, val y: Double, val rotation: Double, val scaleX: Double, val scaleY: Double)
-
 /**
  *
  * @author Almas Baimagambetov (almaslvl@gmail.com)
@@ -196,19 +193,34 @@ class TransformComponent(x: Double, y: Double, angle: Double, scaleX: Double, sc
     }
 
     override fun toString(): String {
-        return "Transform(" + ")"
+        return "Transform($x, $y, $angle, $scaleX, $scaleY)"
     }
 
     override fun write(bundle: Bundle) {
         bundle.put("propX", x)
         bundle.put("propY", y)
+        bundle.put("angle", angle)
+        bundle.put("scaleX", scaleX)
+        bundle.put("scaleY", scaleY)
+        bundle.put("scaleOriginX", scaleOrigin.x)
+        bundle.put("scaleOriginY", scaleOrigin.y)
+        bundle.put("rotationOriginX", rotationOrigin.x)
+        bundle.put("rotationOriginY", rotationOrigin.y)
     }
 
     override fun read(bundle: Bundle) {
         setPosition(bundle.get("propX"), bundle.get("propY"))
+        scaleOrigin = Point2D(bundle.get("scaleOriginX"), bundle.get("scaleOriginY"))
+        rotationOrigin = Point2D(bundle.get("rotationOriginX"), bundle.get("rotationOriginY"))
+        angle = bundle.get("angle")
+        scaleX = bundle.get("scaleX")
+        scaleY = bundle.get("scaleY")
     }
 
     override fun copy(): TransformComponent {
-        return TransformComponent(x, y, angle, scaleX, scaleY)
+        val c = TransformComponent(x, y, angle, scaleX, scaleY)
+        c.scaleOrigin = scaleOrigin
+        c.rotationOrigin = rotationOrigin
+        return c
     }
 }
--- fxgl-entity/src/test/kotlin/com/almasb/fxgl/entity/components/TransformComponentTest.kt
+++ fxgl-entity/src/test/kotlin/com/almasb/fxgl/entity/components/TransformComponentTest.kt
@@ -6,18 +6,102 @@
 
 package com.almasb.fxgl.entity.components
 
-import com.almasb.fxgl.entity.Entity
 import com.almasb.fxgl.core.serialization.Bundle
 import javafx.geometry.Point2D
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.jupiter.api.Assertions.assertAll
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.function.Executable
 
-class PositionComponentTest {
+class TransformComponentTest {
+
+    @Test
+    fun `Serialization`() {
+        val c = TransformComponent(Point2D(30.0, 55.0))
+        c.scaleOrigin = Point2D(1.0, 1.0)
+        c.scaleX = 1.5
+        c.scaleY = 3.0
+        c.angle = 45.0
+        c.rotationOrigin = Point2D(2.0, 2.0)
+
+        val bundle = Bundle("test")
+        c.write(bundle)
+
+        val c2 = TransformComponent()
+        c2.read(bundle)
+
+        assertTrue(areEqual(c, c2))
+    }
+
+    @Test
+    fun `Copy`() {
+        val c = TransformComponent(Point2D(30.0, 55.0))
+        c.scaleOrigin = Point2D(1.0, 1.0)
+        c.scaleX = 1.5
+        c.scaleY = 3.0
+        c.angle = 45.0
+        c.rotationOrigin = Point2D(2.0, 2.0)
+
+        val c2 = c.copy()
+
+        assertTrue(areEqual(c, c2))
+    }
+
+    @Test
+    fun `To String`() {
+        val c = TransformComponent(100.0, 100.0, 30.0, 2.0, 2.0)
+
+        assertThat(c.toString(), `is`("Transform(100.0, 100.0, 30.0, 2.0, 2.0)"))
+    }
+
+    //
+//    @Test
+//    fun `Creation`() {
+//        val rot1 = RotationComponent(35.0)
+//
+//        assertThat(rot1.value, `is`(35.0))
+//    }
+//
+//    @Test
+//    fun `Copy`() {
+//        val rot1 = RotationComponent(35.0)
+//        val rot2 = rot1.copy()
+//
+//        assertThat(rot2.value, `is`(35.0))
+//        assertTrue(rot1 !== rot2)
+//    }
+//
+//    @Test
+//    fun `Rotation`() {
+//        val rot1 = RotationComponent(35.0)
+//
+//        rot1.rotateBy(30.0)
+//        assertThat(rot1.value, `is`(65.0))
+//
+//        rot1.rotateBy(-65.0)
+//        assertThat(rot1.value, `is`(0.0))
+//
+//        rot1.rotateToVector(Point2D(-1.0, 0.0))
+//        assertThat(rot1.value, `is`(180.0))
+//
+//        rot1.rotateToVector(Point2D(1.0, 0.0))
+//        assertThat(rot1.value, `is`(0.0))
+//
+//        rot1.rotateToVector(Point2D(0.0, -1.0))
+//        assertThat(rot1.value, `is`(-90.0))
+//
+//        rot1.rotateToVector(Point2D(0.0, 1.0))
+//        assertThat(rot1.value, `is`(90.0))
+//    }
+//
+
+    private fun areEqual(t1: TransformComponent, t2: TransformComponent): Boolean {
+        return t1.x == t2.x && t1.y == t2.y
+                && t1.scaleX == t2.scaleX && t1.scaleY == t2.scaleY && t1.scaleOrigin == t2.scaleOrigin
+                && t1.angle == t2.angle && t1.rotationOrigin == t2.rotationOrigin
+    }
+
 //
 //    private lateinit var position: PositionComponent
 //
@@ -93,26 +177,5 @@ class PositionComponentTest {
 //        position.translateTowards(Point2D(20.0, 0.0), 5.0)
 //
 //        assertThat(position.value, `is`(Point2D(5.0, 0.0)))
-//    }
-//
-//    @Test
-//    fun `Copy`() {
-//        position = PositionComponent(Point2D(33.0, -33.0))
-//        val copy = position.copy()
-//
-//        assertThat(copy.value, `is`(Point2D(33.0, -33.0)))
-//    }
-//
-//    @Test
-//    fun `Save and load`() {
-//        position.value = Point2D(33.0, -33.0)
-//
-//        val bundle = Bundle("Test")
-//        position.write(bundle)
-//
-//        val position2 = PositionComponent()
-//        position2.read(bundle)
-//
-//        assertThat(position2.value, `is`(Point2D(33.0, -33.0)))
 //    }
 }
