af75f8cf2077ef9688b5b8b74bedc53265bb139f
Almas Baimagambetov <almaslvl@gmail.com>
2019-03-14 13:46:39 +0000

    missed lines

---
+++ fxgl-core/src/test/kotlin/com/almasb/fxgl/core/util/EmptyRunnableTest.kt
@@ -0,0 +1,24 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package com.almasb.fxgl.core.util
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+/**
+ *
+ *
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+class EmptyRunnableTest {
+
+    @Test
+    fun `Run`() {
+        EmptyRunnable.run()
+    }
+}
