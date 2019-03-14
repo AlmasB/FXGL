6836287650c19f182ca2cd48aef22fb3d4abf750
Almas Baimagambetov <almaslvl@gmail.com>
2019-03-14 13:28:06 +0000

    added basic particle emitter test

---
+++ fxgl-effects/src/test/kotlin/com/almasb/fxgl/particle/ParticleEmitterTest.kt
@@ -0,0 +1,32 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package com.almasb.fxgl.particle
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+/**
+ *
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+class ParticleEmitterTest {
+
+    @Test
+    fun `das`() {
+        val emitter = ParticleEmitter()
+        emitter.maxEmissions = 1
+        emitter.numParticles = 22
+
+        val particles = emitter.emit(10.0, 10.0)
+
+
+        assertThat(particles.size(), `is`(22))
+    }
+}
