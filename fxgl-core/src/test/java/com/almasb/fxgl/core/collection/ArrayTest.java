5587334b7a2d0deac1e24c7368636f8d2d1f8a6d
Lewis-Allen <lewisn11@hotmail.co.uk>
2019-03-15 18:12:46 +0100

    Added public modifier to Array Test class.

--- fxgl-core/src/test/java/com/almasb/fxgl/core/collection/ArrayTest.java
+++ fxgl-core/src/test/java/com/almasb/fxgl/core/collection/ArrayTest.java
@@ -1,13 +1,10 @@
 package com.almasb.fxgl.core.collection;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class ArrayTest
+public class ArrayTest
 {
     @Test
     public void constructor_should_instantiate()
