024a6515b6b5f1ca1bbeb0164c27c42eebbd37c4
Almas Baimagambetov <almaslvl@gmail.com>
2019-04-20 21:22:20 +0100

    added missing test case

--- fxgl-io/src/test/kotlin/com/almasb/fxgl/io/FSTest.kt
+++ fxgl-io/src/test/kotlin/com/almasb/fxgl/io/FSTest.kt
@@ -166,7 +166,7 @@ class FSTest {
         val data = FS.loadLastModifiedFileTask<String>("testdir", false).run()
         assertThat(data, `is`("b"))
 
-        val data2 = FS.loadLastModifiedFileTask<String>("testdir", true).run()
+        val data2 = FS.loadLastModifiedFileTask<String>("testdir/", true).run()
         assertThat(data2, `is`("b"))
 
         deleteIfExists(path("testdir/file.a"))
