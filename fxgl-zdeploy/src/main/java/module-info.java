84dbc74e5ddc5e06e6a1d0c9399c34e4282fe2ef
Almas Baimagambetov <almaslvl@gmail.com>
2019-09-03 18:14:24 +0100

    added module-info for javadoc to succeed

---
+++ fxgl-zdeploy/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+module com.almasb.fxgl.zdeploy {
+}
