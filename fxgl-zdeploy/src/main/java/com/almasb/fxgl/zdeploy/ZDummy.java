9987a050fd976a35b367d38251fab6bfcb51fa1a
Almas Baimagambetov <almaslvl@gmail.com>
2019-09-17 21:31:34 +0100

    use empty javadoc jar since javadoc and dokka cause lots of problems during site build and are not yet optimised for jdk 11+

---
+++ fxgl-zdeploy/src/main/java/com/almasb/fxgl/zdeploy/ZDummy.java
@@ -0,0 +1,13 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package com.almasb.fxgl.zdeploy;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public class ZDummy {
+}
