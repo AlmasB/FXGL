68dd8b3c83510bfae5ce7c260d90bd37a4664c8b
marvinbuff <marvin.buff@optravis.com>
2019-11-12 07:59:13 +0100

    Remove duplicated properties and implement requested changes.

--- fxgl/src/main/kotlin/com/almasb/fxgl/dsl/components/view/GenericBarViewComponent.kt
+++ fxgl/src/main/kotlin/com/almasb/fxgl/dsl/components/view/GenericBarViewComponent.kt
@@ -8,32 +8,47 @@ package com.almasb.fxgl.dsl.components.view
 
 import com.almasb.fxgl.ui.Position
 import com.almasb.fxgl.ui.ProgressBar
-import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.DoubleProperty
 import javafx.scene.paint.Color
 
 /**
  * A general bar view component to display properties like health, mana, or energy.
- * Can be fully customized using its [bar] and [maxValue] property.
+ * Can be fully customized using its [bar] property.
  * @author Marvin Buff (marvinbuff@hotmail.com)
  */
-open class GenericBarViewComponent(x: Double, y: Double, valueProperty: SimpleIntegerProperty, color: Color) : ChildViewComponent(x, y, false) {
+open class GenericBarViewComponent constructor(
+        x: Double,
+        y: Double,
+        color: Color,
+        initialValue: Double,
+        maxValue: Double = initialValue
+) : ChildViewComponent(x, y, false) {
+
+    /**
+     * Utility constructor to setup the [GenericBarViewComponent] bound to the given [property].
+     */
+    constructor(x: Double, y: Double, color: Color, property: DoubleProperty
+    ) : this(x, y, color, property.value) {
+        valueProperty().bind(property)
+    }
 
     val bar = ProgressBar()
-    val maxValue = SimpleIntegerProperty(valueProperty.get())
 
     init {
 
         bar.setMinValue(0.0)
-        bar.setMaxValue(100.0)
+        bar.setMaxValue(maxValue)
         bar.setWidth(100.0)
         bar.isLabelVisible = false
         bar.setLabelPosition(Position.RIGHT)
         bar.setFill(color)
-
-        bar.currentValueProperty().bind(
-                valueProperty.divide(maxValue).multiply(100)
-        )
+        bar.currentValue = initialValue
 
         viewRoot.children.add(bar)
+
     }
+
+    fun valueProperty(): DoubleProperty = bar.currentValueProperty()
+    fun maxValueProperty(): DoubleProperty = bar.maxValueProperty()
+
 }
