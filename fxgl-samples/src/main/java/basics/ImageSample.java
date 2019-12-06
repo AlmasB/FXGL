fcd10ecc0cbb398ed1707e4a274ad7279e9da747
CharlyZhu <charlyzhu@hotmail.com>
2019-12-06 20:30:43 +0000

    1. Provided method resize in Images.kt 2. Added ImageSample.java as demo

--- fxgl-samples/src/main/java/basics/ImageSample.java
+++ fxgl-samples/src/main/java/basics/ImageSample.java
@@ -3,6 +3,7 @@ package basics;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.texture.ColoredTexture;
 import com.almasb.fxgl.texture.ImagesKt;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,19 +12,30 @@ public class ImageSample extends GameApplication {
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setWidth(800);
-        settings.setHeight(600);
+        settings.setHeight(800);
         settings.setTitle("ImageSample");
     }
 
     @Override
     protected void initGame() {
-        Image original = new Image("https://www.empiraft.com/zh_cn/new/images/background.png");
-        Image processed = ImagesKt.resize(original, 300, 200);
-        FXGL.debug(processed.getWidth() + "-" + processed.getHeight());
+        // Image obtained from internet.
+        Image background = new Image("https://www.empiraft.com/zh_cn/new/images/background.png");
+        Image backgroundResized = ImagesKt.resize(background, 400, 200);
         FXGL.entityBuilder()
-                .at(0, 0)
-                .view(new ImageView(processed))
+                .at(400, 0)
+                .view(new ImageView(backgroundResized))
                 .buildAndAttach();
+
+        // Image obtained from internet.
+        Image original = new Image("https://www.empiraft.com/zh_cn/new/images/icon-game.png");
+        // Demo to resize image, note: original image from url ("https://www.empiraft.com/zh_cn/new/images/icon-game.png") is 64px * 64px
+        for (int i = 16; i > 0; i--) {
+            Image processed = ImagesKt.resize(original, 16 * i, 16 * i);
+            FXGL.entityBuilder()
+                    .at(5 * i  * i, 0.5 * i * i * i)
+                    .view(new ImageView(processed))
+                    .buildAndAttach();
+        }
     }
 
     public static void main(String[] args) {
