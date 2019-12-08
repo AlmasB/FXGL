fc804c3ecf91f5426dff8ecfa6f980f431eff1b5
CharlyZhu <charlyzhu@hotmail.com>
2019-12-08 17:17:20 +0000

    added license stuff

--- fxgl/src/main/kotlin/com/almasb/fxgl/dsl/components/IntervalSwitchComponent.kt
+++ fxgl/src/main/kotlin/com/almasb/fxgl/dsl/components/IntervalSwitchComponent.kt
@@ -1,3 +1,9 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
 package com.almasb.fxgl.dsl.components
 
 import com.almasb.fxgl.dsl.getGameTimer
