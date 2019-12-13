163e5c536819ba32f6b595f91fd03f68435db1f5
AlmasB <almaslvl@gmail.com>
2019-12-13 10:10:23 +0000

    added fxgl-ai module

---
+++ fxgl-ai/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+/**
+ *
+ */
+module com.almasb.fxgl.ai {
+    requires com.almasb.fxgl.core;
+    requires com.almasb.fxgl.entity;
+}
