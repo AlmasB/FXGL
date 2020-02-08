c031334f6d6f21e6b4e368d03410e0907f10713c
Almas Baimagambetov <almaslvl@gmail.com>
2020-02-08 20:42:17 +0000

    fixed interval switch not updating

--- fxgl/src/main/kotlin/com/almasb/fxgl/dsl/components/IntervalSwitchComponent.kt
+++ fxgl/src/main/kotlin/com/almasb/fxgl/dsl/components/IntervalSwitchComponent.kt
@@ -16,7 +16,7 @@ class IntervalSwitchComponent(initValue: Boolean = false,
 
     private val timer: LocalTimer = FXGL.newLocalTimer()
 
-    fun onUpdate() {
+    override fun onUpdate(tpf: Double) {
         // after a certain interval set valueProperty() to false, then to true and so on.
         if (timer.elapsed(interval)) {
             value = !value
