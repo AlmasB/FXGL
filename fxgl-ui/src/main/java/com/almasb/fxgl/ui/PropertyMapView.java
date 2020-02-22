9183849b4b0b02f1b091270ea14639a27bf8740a
Almas Baimagambetov <almaslvl@gmail.com>
2020-02-22 18:51:20 +0000

    initial commit to API for producing editable views of custom ObjectProperty values, #704

--- fxgl-ui/src/main/java/com/almasb/fxgl/ui/PropertyMapView.java
+++ fxgl-ui/src/main/java/com/almasb/fxgl/ui/PropertyMapView.java
@@ -7,6 +7,8 @@
 package com.almasb.fxgl.ui;
 
 import com.almasb.fxgl.core.collection.PropertyMap;
+import com.almasb.fxgl.core.collection.UpdatableObjectProperty;
+import com.almasb.fxgl.core.math.Vec2;
 import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -23,15 +25,95 @@ import javafx.util.converter.DefaultStringConverter;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.IntegerStringConverter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Charly Zhu (charlyzhu@hotmail.com)
  */
 public class PropertyMapView extends Parent {
 
+    private static final Map<Class<?>, PropertyViewChangeListener<?, ?>> converters = new HashMap<>();
+
+    static {
+        // TODO: abstract superclass to remove duplicate code?
+        PropertyViewChangeListener<Vec2, HBox> l = new PropertyViewChangeListener<>() {
+
+            private boolean ignoreChangeView = false;
+            private boolean ignoreChangeProperty = false;
+
+            @Override
+            public HBox makeView(ObjectProperty<Vec2> value) {
+                var fieldX = new TextField();
+                var fieldY = new TextField();
+                HBox view = new HBox(fieldX, fieldY);
+
+                value.addListener((obs, o, newValue) -> {
+                    if (ignoreChangeProperty)
+                        return;
+
+                    onPropertyChanged(value, view);
+                });
+
+                fieldX.textProperty().addListener((obs, o, x) -> {
+                    if (ignoreChangeView)
+                        return;
+
+                    onViewChanged(value, view);
+                });
+
+                fieldY.textProperty().addListener((obs, o, y) -> {
+                    if (ignoreChangeView)
+                        return;
+
+                    onViewChanged(value, view);
+                });
+
+                onPropertyChanged(value, view);
+
+                return view;
+            }
+
+            @Override
+            public void onPropertyChanged(ObjectProperty<Vec2> value, HBox view) {
+                var fieldX = (TextField) view.getChildren().get(0);
+                var fieldY = (TextField) view.getChildren().get(1);
+
+                ignoreChangeView = true;
+
+                fieldX.setText(Float.toString(value.getValue().x));
+                fieldY.setText(Float.toString(value.getValue().y));
+
+                ignoreChangeView = false;
+            }
+
+            @Override
+            public void onViewChanged(ObjectProperty<Vec2> value, HBox view) {
+                var fieldX = (TextField) view.getChildren().get(0);
+                var fieldY = (TextField) view.getChildren().get(1);
+
+                ignoreChangeProperty = true;
+
+                value.getValue().x = Float.parseFloat(fieldX.getText());
+                value.getValue().y = Float.parseFloat(fieldY.getText());
+
+                ((UpdatableObjectProperty<Vec2>)value).forceUpdateListeners(value.getValue(), value.getValue());
+
+                ignoreChangeProperty = false;
+            }
+        };
+
+        addViewConverter(Vec2.class, l);
+    }
+
+    public static <T> void addViewConverter(Class<T> type, PropertyViewChangeListener<T, ?> converter) {
+        converters.put(type, converter);
+    }
+
     public PropertyMapView(PropertyMap map) {
         VBox rootBox = new VBox();
         rootBox.setBackground(new Background(new BackgroundFill(Color.gray(1.0), CornerRadii.EMPTY, Insets.EMPTY)));
-        
+
         for (String key : map.keys()) {
             HBox propertyBox = new HBox();
         
@@ -46,8 +128,10 @@ public class PropertyMapView extends Parent {
 
         getChildren().add(rootBox);
     }
-    
+
     /**
+     * // TODO: cleanup using general converters
+     *
      * Makes a check box when it is a boolean value and text field when it is string etc.
      */
     @SuppressWarnings("unchecked")
@@ -56,8 +140,7 @@ public class PropertyMapView extends Parent {
             CheckBox box = new CheckBox();
             box.selectedProperty().bindBidirectional((BooleanProperty) value);
             return box;
-        }
-        else if (value instanceof ObjectProperty) {
+        } else if (value instanceof ObjectProperty) {
             ObjectProperty<?> property = (ObjectProperty<?>) value;
             
             // If object is an enum.
@@ -65,11 +148,20 @@ public class PropertyMapView extends Parent {
                 return makeEnumView((ObjectProperty<Enum<?>>) property);
             
             // If object is something else.
-            Text text = new Text();
-            text.textProperty().bind(property.asString());
-            return text;
-        }
-        else {
+            if (converters.containsKey(property.get().getClass())) {
+                PropertyViewChangeListener<?, ?> converter = converters.get(property.get().getClass());
+
+                return converter.makeViewInternal(property);
+
+            } else {
+                // If object does not have a converter.
+
+                Text text = new Text();
+                text.textProperty().bind(property.asString());
+                return text;
+            }
+
+        } else {
             TextField textField = new TextField();
             textField.setPrefWidth(150);
             
