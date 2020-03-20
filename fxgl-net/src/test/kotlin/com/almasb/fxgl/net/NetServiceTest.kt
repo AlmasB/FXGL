1b4dee6ce5bf8ae6dd1959210994bab33359f666
Almas Baimagambetov <almaslvl@gmail.com>
2020-03-20 09:35:03 +0000

    added missing test

--- fxgl-net/src/test/kotlin/com/almasb/fxgl/net/NetServiceTest.kt
+++ fxgl-net/src/test/kotlin/com/almasb/fxgl/net/NetServiceTest.kt
@@ -6,10 +6,35 @@
 
 package com.almasb.fxgl.net
 
+import org.hamcrest.MatcherAssert
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
+
 /**
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
 class NetServiceTest {
 
+    private lateinit var net: NetService
 
+    @BeforeEach
+    fun setUp() {
+        net = NetService()
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "CI", matches = "true")
+    fun `Open stream to URL`() {
+        val numLines = net.openStreamTask("https://raw.githubusercontent.com/AlmasB/FXGL/master/README.md")
+                .run()
+                .bufferedReader()
+                .lines()
+                .count()
+
+        assertThat(numLines, greaterThan(0L))
+    }
 }
