d8dbaa35fab5cb33e0863e9b45e4e799fb85cf70
Almas Baimagambetov <almaslvl@gmail.com>
2020-05-08 10:58:40 +0100

    restored SaveLoadServiceTest

--- fxgl-profiles/src/test/kotlin/com/almasb/fxgl/profile/SaveLoadServiceTest.kt
+++ fxgl-profiles/src/test/kotlin/com/almasb/fxgl/profile/SaveLoadServiceTest.kt
@@ -10,19 +10,15 @@ import com.almasb.fxgl.core.serialization.Bundle
 import com.almasb.fxgl.io.FileSystemService
 import com.almasb.fxgl.test.InjectInTest
 import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers
 import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.lang.invoke.MethodHandles
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.time.LocalDateTime
 
 /**
  *
@@ -32,84 +28,80 @@ import java.time.LocalDateTime
 class SaveLoadServiceTest {
 
     private lateinit var saveLoadService: SaveLoadService
-    // TODO:
-//
-//    companion object {
-//        @BeforeAll
-//        @JvmStatic fun before() {
-//            cleanUp()
-//        }
-//
-//        @AfterAll
-//        @JvmStatic fun cleanUp() {
-//
-//            // ensure previous tests have been cleared
-//            Paths.get("profiles/").toFile().deleteRecursively()
-//
-//            assertTrue(!Files.exists(Paths.get("profiles/")), "Profiles dir is present before")
-//        }
-//    }
-//
-//    @BeforeEach
-//    fun setUp() {
-//        saveLoadService = SaveLoadService()
-//
-//        InjectInTest.inject(MethodHandles.lookup(), saveLoadService, "fs", FileSystemService().also { it.onInit() })
-//    }
-//
-//    @Test
-//    fun `Test new - save - load`() {
-//        // we do a full clean before so it's important that
-//        // we keep the order, i.e. we save first so we have something to load from
-//        `Save game data`()
-//        `Load game data`()
-//        `Read file names`()
-//        `Delete game data`()
-//        `Delete profile`()
-//    }
-//
-//    fun `Save game data`() {
-//        val bundle1 = Bundle("Hello")
-//        bundle1.put("id", 9)
-//
-//        val data1 = DataFile()
-//        data1.putBundle(bundle1)
-//
-//        val save1 = SaveFile("TestSave", "TestProfileName", "sav", LocalDateTime.now(), data1)
-//
-//        saveLoadService.writeSaveFileTask(save1).run()
-//
-//        assertTrue(saveLoadService.saveFileExists(SaveFile("TestSave", "TestProfileName", "sav")))
-//        assertFalse(saveLoadService.saveFileExists(SaveFile("TestSave2", "TestProfileName", "sav")))
-//        assertTrue(Files.exists(Paths.get("profiles/TestProfileName/TestSave.sav")), "Save file was not created")
-//    }
-//
-//    fun `Load game data`() {
-//        val saveFile = saveLoadService.readSaveFileTask(SaveFile("TestSave", "TestProfileName", "sav")).run()
-//
-//        assertThat(saveFile, `is`(notNullValue()))
-//
-//        assertThat(saveFile.data.getBundle("Hello").get("id"), `is`(9))
-//    }
-//
-//    fun `Read file names`() {
-//        val profileNames = saveLoadService.readProfileNamesTask().run()
-//
-//        assertThat(profileNames, Matchers.contains("TestProfileName"))
-//
-//        val saveFiles = saveLoadService.readSaveFilesTask("TestProfileName", "sav").run()
-//
-//        assertThat(saveFiles.size, `is`(1))
-//        assertThat(saveFiles[0].name, `is`("TestSave"))
-//    }
-//
-//    fun `Delete game data`() {
-//        assertTrue(saveLoadService.saveFileExists(SaveFile("TestSave", "TestProfileName", "sav")))
-//
-//        saveLoadService.deleteSaveFileTask(SaveFile("TestSave", "TestProfileName", "sav")).run()
-//
-//        assertFalse(saveLoadService.saveFileExists(SaveFile("TestSave", "TestProfileName", "sav")))
-//    }
+
+    companion object {
+        @BeforeAll
+        @JvmStatic fun before() {
+            cleanUp()
+        }
+
+        @AfterAll
+        @JvmStatic fun cleanUp() {
+
+            // ensure previous tests have been cleared
+            Paths.get("profiles/").toFile().deleteRecursively()
+
+            assertTrue(!Files.exists(Paths.get("profiles/")), "Profiles dir is present before")
+        }
+    }
+
+    @BeforeEach
+    fun setUp() {
+        saveLoadService = SaveLoadService()
+
+        InjectInTest.inject(MethodHandles.lookup(), saveLoadService, "fs", FileSystemService().also { it.onInit() })
+    }
+
+    @Test
+    fun `Test write - read`() {
+        // we do a full clean before so it's important that
+        // we keep the order, i.e. we save first so we have something to load from
+        `Write game data`()
+        `Read game data`()
+        `Read file names`()
+        `Delete game data`()
+        //`Delete profile`()
+    }
+
+    fun `Write game data`() {
+        val bundle1 = Bundle("Hello")
+        bundle1.put("id", 9)
+
+        val data1 = DataFile()
+        data1.putBundle(bundle1)
+
+        saveLoadService.writeTask("profiles/TestSave.sav", data1).run()
+
+        assertTrue(saveLoadService.saveFileExists("profiles/TestSave.sav"))
+    }
+
+    fun `Read game data`() {
+        val saveFile = saveLoadService.readTask("profiles/TestSave.sav").run()
+
+        assertNotNull(saveFile)
+
+        assertThat(saveFile.data.getBundle("Hello").get("id"), `is`(9))
+    }
+
+    fun `Read file names`() {
+        saveLoadService.writeTask("profiles/TestSave2.sav", DataFile()).run()
+
+        assertTrue(saveLoadService.saveFileExists("profiles/TestSave2.sav"))
+
+        val saveFiles = saveLoadService.readSaveFilesTask("profiles", "sav").run()
+
+        assertThat(saveFiles.size, `is`(2))
+        assertThat(saveFiles[0].name, `is`("profiles/TestSave.sav"))
+        assertThat(saveFiles[1].name, `is`("profiles/TestSave2.sav"))
+    }
+
+    fun `Delete game data`() {
+        assertTrue(saveLoadService.saveFileExists("profiles/TestSave.sav"))
+
+        saveLoadService.deleteSaveFileTask("profiles/TestSave.sav").run()
+
+        assertFalse(saveLoadService.saveFileExists("profiles/TestSave.sav"))
+    }
 //
 //    fun `Delete profile`() {
 //        assertTrue(Files.exists(Paths.get("profiles/TestProfileName")))
