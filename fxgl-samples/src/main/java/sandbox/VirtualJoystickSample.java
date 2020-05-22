22b627a45c0be3cef971061868ac65268a4fc2fd
Almas Baimagambetov <almaslvl@gmail.com>
2020-05-22 10:32:32 +0100

    updated sample to use virtual joystick

--- fxgl-samples/src/main/java/sandbox/VirtualJoystickSample.java
+++ fxgl-samples/src/main/java/sandbox/VirtualJoystickSample.java
@@ -8,10 +8,14 @@ package sandbox;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.dsl.components.AutoRotationComponent;
+import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.input.virtual.VirtualJoystick;
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 /**
  *
@@ -19,23 +23,40 @@ import javafx.scene.text.Text;
 public class VirtualJoystickSample extends GameApplication {
 
     @Override
-    protected void initSettings(GameSettings settings) { }
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(1280);
+        settings.setHeight(720);
+    }
 
     private VirtualJoystick joystick;
     private Text debug;
+    private Entity entity;
 
     @Override
     protected void initGame() {
-        joystick = FXGL.getInput().createVirtualJoystick();
-        debug = FXGL.getUIFactoryService().newText("", Color.BLACK, 18.0);
+        getGameScene().setBackgroundColor(Color.LIGHTGRAY);
 
-        FXGL.addUINode(joystick, 50, 400);
-        FXGL.addUINode(debug, 500, 60);
+        joystick = getInput().createVirtualJoystick();
+        debug = getUIFactoryService().newText("", Color.BLACK, 18.0);
+
+        entity = entityBuilder()
+                .at(300, 300)
+                .viewWithBBox(texture("player2rot.png").brighter().outline(Color.BLACK))
+                // TODO: component should convert degrees correctly to avoid sharp turns when moving between 180 and -180
+                .with(new AutoRotationComponent().withSmoothing())
+                .buildAndAttach();
+
+        addUINode(joystick, 50, 400);
+        //FXGL.addUINode(debug, 50, 60);
     }
 
     @Override
     protected void onUpdate(double tpf) {
         debug.setText("Vector: " + joystick.getVector());
+
+        if (!joystick.getVector().equals(Point2D.ZERO)) {
+            entity.translate(joystick.getVector().multiply(3));
+        }
     }
 
     public static void main(String[] args) {
