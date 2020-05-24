31be3d635ba4cbc333bb84579165dc573df57876
Almas Baimagambetov <almaslvl@gmail.com>
2020-05-24 11:17:24 +0100

    fixed todo

--- fxgl/src/test/kotlin/com/almasb/fxgl/app/EngineTest.kt
+++ fxgl/src/test/kotlin/com/almasb/fxgl/app/EngineTest.kt
@@ -8,18 +8,15 @@ package com.almasb.fxgl.app
 
 import com.almasb.fxgl.core.EngineService
 import com.almasb.fxgl.core.Inject
-import com.almasb.fxgl.core.concurrent.Async
 import com.almasb.fxgl.test.RunWithFX
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.*
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.extension.ExtendWith
-import java.lang.Exception
-import java.lang.IllegalArgumentException
 import java.time.Duration
 import java.util.concurrent.CountDownLatch
+import kotlin.IllegalArgumentException
 
 /**
  *
@@ -56,62 +53,47 @@ class EngineTest {
         }
     }
 
-    // TODO:
-//    @Test
-//    fun `Check dependencies are injected into services`() {
-//        engine.environmentVars["name"] = "Hello World FXGL"
-//
-//        engine.initServicesAndStartLoop()
-//
-//        assertTimeoutPreemptively(Duration.ofSeconds(3)) {
-//            latch.await()
-//
-//            val service1 = engine.getService(TestService1::class.java)
-//
-//            assertThat(service1.name, `is`("Hello World FXGL"))
-//            assertTrue(service1.service === engine.getService(TestService2::class.java))
-//            assertThat(service1.count, `is`(1))
-//
-//            engine.stopLoopAndExitServices()
-//
-//            assertThat(service1.count, `is`(2))
-//        }
-//    }
+    @Test
+    fun `Check dependencies are injected into services`() {
+        engine.environmentVars["name"] = "Hello World FXGL"
 
-//    @Test
-//    fun `Failed dependencies are thrown to JavaFX thread`() {
-//        engine = Engine(GameSettings().also {
-//            it.engineServices.clear()
-//            it.engineServices.addAll(arrayOf(FailService::class.java))
-//        }.toReadOnly())
-//
-//        var error: Throwable? = null
-//
-//        var oldHandler: Thread.UncaughtExceptionHandler? = null
-//
-//        val l = CountDownLatch(1)
-//
-//        Async.startAsyncFX {
-//            oldHandler = Thread.getDefaultUncaughtExceptionHandler()
-//
-//            Thread.setDefaultUncaughtExceptionHandler { _, e ->
-//                error = e
-//                l.countDown()
-//            }
-//        }.await()
-//
-//        engine.initServicesAndStartLoop()
-//
-//        assertTimeoutPreemptively(Duration.ofSeconds(3)) {
-//            l.await()
-//
-//            Async.startAsyncFX {
-//                Thread.setDefaultUncaughtExceptionHandler(oldHandler)
-//            }.await()
-//
-//            assertNotNull(error)
-//        }
-//    }
+        engine.initServices()
+
+        assertTimeoutPreemptively(Duration.ofSeconds(3)) {
+            latch.await()
+
+            val service1 = engine.getService(TestService1::class.java)
+
+            assertThat(service1.name, `is`("Hello World FXGL"))
+            assertTrue(service1.service === engine.getService(TestService2::class.java))
+            assertThat(service1.count, `is`(1))
+        }
+    }
+
+    @Test
+    fun `Failed dependencies are thrown`() {
+        engine = Engine(GameSettings().also {
+            it.engineServices.clear()
+            it.engineServices.addAll(arrayOf(FailService1::class.java))
+        }.toReadOnly())
+
+        var e: Exception = assertThrows<IllegalStateException> {
+            engine.initServices()
+        }
+
+        assertThat(e.message, containsString("No provider found"))
+
+        engine = Engine(GameSettings().also {
+            it.engineServices.clear()
+            it.engineServices.addAll(arrayOf(FailService2::class.java))
+        }.toReadOnly())
+
+        e = assertThrows<IllegalArgumentException> {
+            engine.initServices()
+        }
+
+        assertThat(e.message, containsString("No value present"))
+    }
 
     class TestService1 : EngineService() {
 
@@ -138,7 +120,12 @@ class EngineTest {
         }
     }
 
-    class FailService : EngineService() {
+    class FailService1 : EngineService() {
         lateinit var service: TestService2
+    }
+
+    class FailService2 : EngineService() {
+        @Inject("bla-bla")
+        lateinit var name: String
     }
 }
