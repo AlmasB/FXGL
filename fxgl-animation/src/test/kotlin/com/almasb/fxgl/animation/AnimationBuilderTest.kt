aea890d3b9535342cdae632d5f5b946c55878498
CharlyZhu <charlyzhu@hotmail.com>
2020-06-10 13:27:17 +0100

    Added test functions Rotate, Scale and Fade

--- fxgl-animation/src/test/kotlin/com/almasb/fxgl/animation/AnimationBuilderTest.kt
+++ fxgl-animation/src/test/kotlin/com/almasb/fxgl/animation/AnimationBuilderTest.kt
@@ -41,6 +41,42 @@ class AnimationBuilderTest {
     }
 
     @Test
+    fun `Scale`() {
+        val anim = builder.scale(e)
+                .from(Point2D(1.0, 1.0))
+                .to(Point2D(3.0, 3.0))
+                .build()
+
+        anim.start()
+
+        assertThat(e.scaleX, `is`(1.0))
+        assertThat(e.scaleY, `is`(1.0))
+
+        anim.onUpdate(0.5)
+        anim.onUpdate(0.5)
+
+        assertThat(e.scaleX, `is`(3.0))
+        assertThat(e.scaleY, `is`(3.0))
+    }
+
+    @Test
+    fun `Rotate`() {
+        val anim = builder.rotate(e)
+                .from(0.0)
+                .to(180.0)
+                .build()
+
+        anim.start()
+
+        assertThat(e.rotation, `is`(0.0))
+
+        anim.onUpdate(0.5)
+        anim.onUpdate(0.5)
+
+        assertThat(e.rotation, `is`(180.0))
+    }
+
+    @Test
     fun `Translate`() {
         val anim = builder.translate(e)
                 .from(Point2D(10.0, 10.0))
@@ -57,5 +93,22 @@ class AnimationBuilderTest {
 
         assertThat(e.x, `is`(50.0))
         assertThat(e.y, `is`(50.0))
+    }
+
+    @Test
+    fun `Fade`() {
+        val anim = builder.fade(e)
+                .from(0.0)
+                .to(1.0)
+                .build()
+
+        anim.start()
+
+        assertThat(e.opacity, `is`(0.0))
+
+        anim.onUpdate(0.5)
+        anim.onUpdate(0.5)
+
+        assertThat(e.opacity, `is`(1.0))
     }
 }
