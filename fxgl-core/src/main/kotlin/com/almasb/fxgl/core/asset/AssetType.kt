9a683c9309fcfa2a42f4e3e3546f17e87e261aa4
Almas Baimagambetov <almaslvl@gmail.com>
2020-07-05 14:00:59 +0100

    added a generic asset loader service for modules that cannot access fxgl module directly

---
+++ fxgl-core/src/main/kotlin/com/almasb/fxgl/core/asset/AssetType.kt
@@ -0,0 +1,15 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package com.almasb.fxgl.core.asset
+
+/**
+ *
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+enum class AssetType {
+    DIALOGUE, TEXTURE, MUSIC, SOUND
+}
