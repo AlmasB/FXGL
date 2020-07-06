9026d9b2afc8901b1ca0282b1712fb479518b2bb
Almas Baimagambetov <almaslvl@gmail.com>
2020-07-06 18:05:56 +0100

    added serialization test

---
+++ fxgl-cutscene/src/test/kotlin/com/almasb/fxgl/cutscene/dialogue/SerializableDialogueGraphTest.kt
@@ -0,0 +1,40 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package com.almasb.fxgl.cutscene.dialogue
+
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers.*
+import org.junit.jupiter.api.Test
+
+/**
+ *
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+class SerializableDialogueGraphTest {
+
+    @Test
+    fun `Serialization to and from`() {
+        val start = StartNode("test start")
+        val choice = ChoiceNode("test choice")
+        val function = FunctionNode("test function")
+        val end = EndNode("test end")
+
+        val graph = DialogueGraph()
+        graph.addNode(start)
+        graph.addNode(choice)
+        graph.addNode(function)
+        graph.addNode(end)
+
+        val sGraph = DialogueGraphSerializer.toSerializable(graph)
+
+        val copy = DialogueGraphSerializer.fromSerializable(sGraph)
+
+        assertThat(copy.uniqueID, `is`(graph.uniqueID))
+        assertThat(copy.nodes.mapValues { it.toString() }, `is`(graph.nodes.mapValues { it.toString() }))
+        assertThat(copy.edges.map { it.toString() }, `is`(graph.edges.map { it.toString() }))
+    }
+}
