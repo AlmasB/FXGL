f51b149b71ccb99cf69fdd12eb15765cf710c5de
AahzBrut <partyomenko@gmail.com>
2020-07-31 15:41:33 +0300

    Replace assertTimeout with assertTimeoutPreemptively

--- fxgl/src/test/kotlin/com/almasb/fxgl/app/services/IOTaskExecutorServiceTest.kt
+++ fxgl/src/test/kotlin/com/almasb/fxgl/app/services/IOTaskExecutorServiceTest.kt
@@ -3,36 +3,25 @@
  * Copyright (c) AlmasB (almaslvl@gmail.com).
  * See LICENSE for details.
  */
-
+@file:Suppress("JAVA_MODULE_DOES_NOT_DEPEND_ON_MODULE")
 package com.almasb.fxgl.app.services
 
 import com.almasb.fxgl.app.MockDialogService
 import com.almasb.fxgl.core.concurrent.IOTask
-import com.almasb.fxgl.net.NetService
 import com.almasb.fxgl.test.InjectInTest
 import com.almasb.fxgl.test.RunWithFX
-import com.almasb.fxgl.ui.DialogBox
-import com.almasb.fxgl.ui.DialogService
 import javafx.application.Platform
-import javafx.beans.property.DoubleProperty
-import javafx.scene.Node
-import javafx.scene.control.Button
-import javafx.util.Duration
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
+import org.junit.jupiter.api.assertTimeoutPreemptively
 import org.junit.jupiter.api.extension.ExtendWith
-import java.lang.RuntimeException
 import java.lang.invoke.MethodHandles
 import java.time.Duration.ofSeconds
 import java.util.concurrent.CountDownLatch
-import java.util.function.Consumer
-import java.util.function.Predicate
 
 /**
  *
@@ -70,7 +59,7 @@ class IOTaskExecutorServiceTest {
 
     @Test
     fun `IOTask runs in a different thread`() {
-        assertTimeout(ofSeconds(1)) {
+        assertTimeoutPreemptively(ofSeconds(1)) {
             var count = 0
             var threadID = -333L
 
@@ -108,7 +97,7 @@ class IOTaskExecutorServiceTest {
 
     @Test
     fun `IOTask Async FX runs in a JavaFX UI thread`() {
-        assertTimeout(ofSeconds(1)) {
+        assertTimeoutPreemptively(ofSeconds(1)) {
             var count = 0
             var threadID = -333L
 
@@ -147,7 +136,7 @@ class IOTaskExecutorServiceTest {
 
     @Test
     fun `IOTask Async FX with dialog success`() {
-        assertTimeout(ofSeconds(1)) {
+        assertTimeoutPreemptively(ofSeconds(1)) {
             val task = IOTask.of {
                 "MyString"
             }
@@ -172,7 +161,7 @@ class IOTaskExecutorServiceTest {
 
     @Test
     fun `IOTask Async FX with dialog fail`() {
-        assertTimeout(ofSeconds(1)) {
+        assertTimeoutPreemptively(ofSeconds(1)) {
             val task = IOTask.of {
                 throw RuntimeException("err")
             }
