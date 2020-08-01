26a6b1552a48e9fd68a3cc24ffc326414ce397bc
Almas Baimagambetov <almaslvl@gmail.com>
2020-08-01 10:52:52 +0100

    added a note why module-info is needed in native samples

--- fxgl-nativesamples/src/main/java/module-info.java
+++ fxgl-nativesamples/src/main/java/module-info.java
@@ -7,7 +7,7 @@
 /**
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
-// TODO: without this, the custom launch fails with javafx 12.0.1
+// module info required for customLaunch to function correctly
 open module com.almasb.fxgl.nativesamples {
     requires com.almasb.fxgl.all;
 }
