9d7af2ce5c167339d1131d1da86dcc93476d6932
Almas Baimagambetov <almaslvl@gmail.com>
2020-08-06 21:00:43 +0100

    cleanup, added missing license header

--- fxgl-tools/src/main/kotlin/com/almasb/fxgl/tools/dialogues/ui/SelectionRectangle.kt
+++ fxgl-tools/src/main/kotlin/com/almasb/fxgl/tools/dialogues/ui/SelectionRectangle.kt
@@ -1,3 +1,9 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
 package com.almasb.fxgl.tools.dialogues.ui
 
 import javafx.scene.Node
@@ -11,8 +17,6 @@ import javafx.scene.shape.Rectangle
  */
 class SelectionRectangle : Rectangle() {
 
-
-
     init {
         fill = Color.web("darkblue", 0.4)
         stroke = Color.LIGHTBLUE
@@ -23,15 +27,8 @@ class SelectionRectangle : Rectangle() {
      * @return partially or fully selected (overlapping) nodes in [root] with [type]
      */
     fun <T : Node> getSelectedNodesIn(root: Parent, type: Class<T>): List<T> {
-        //println("Parent bounds: $boundsInParent")
-
-
         return root.childrenUnmodifiable
                 .filterIsInstance(type)
-                .filter {
-                    //println("Node: ${it.boundsInParent}")
-
-                    it.boundsInParent.intersects(boundsInParent)
-                }
+                .filter { it.boundsInParent.intersects(boundsInParent) }
     }
 }
