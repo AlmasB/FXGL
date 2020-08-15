4c1a9a1c3816cd637772261e45b8001118e16331
Almas Baimagambetov <almaslvl@gmail.com>
2020-08-15 13:14:16 +0100

    skip license and pmd checks during deploy

--- .github/.ci/deploy-sonatype.sh
+++ .github/.ci/deploy-sonatype.sh
@@ -6,6 +6,6 @@ openssl aes-256-cbc -pass pass:$ENC_PASSWORD -in .github/.ci/sign.asc.enc -out .
 
 gpg -q --fast-import .github/.ci/sign.asc
   
-mvn deploy --settings .github/.ci/.travis.settings.xml -DskipTests=true
+mvn deploy --settings .github/.ci/.travis.settings.xml -DskipTests=true -Dlicense.skip=true -Dpmd.skip=true
 
 echo "Deployed to sonatype"
