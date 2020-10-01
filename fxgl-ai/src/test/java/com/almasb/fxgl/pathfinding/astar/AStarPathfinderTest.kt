8aa61d786288d071537631c3b7c5a395e669550d
marvinbuff <marvin.buff@optravis.com>
2020-10-01 14:23:27 +0200

    Convert file to kotlin

--- fxgl-ai/src/test/java/com/almasb/fxgl/pathfinding/astar/AStarPathfinderTest.kt
+++ fxgl-ai/src/test/java/com/almasb/fxgl/pathfinding/astar/AStarPathfinderTest.kt
@@ -3,43 +3,31 @@
  * Copyright (c) AlmasB (almaslvl@gmail.com).
  * See LICENSE for details.
  */
+package com.almasb.fxgl.pathfinding.astar
 
-package com.almasb.fxgl.pathfinding.astar;
+import com.almasb.fxgl.pathfinding.CellState
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
 
-import com.almasb.fxgl.pathfinding.CellState;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Supplier;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class AStarPathfinderTest {
-
-    private static final int GRID_SIZE = 20;
-    private AStarGrid grid;
-    private AStarPathfinder pathfinder;
+class AStarPathfinderTest {
+    private lateinit var grid: AStarGrid
+    private lateinit var pathfinder: AStarPathfinder
 
     @BeforeEach
-    public void setUp() {
-        grid = new AStarGrid(GRID_SIZE, GRID_SIZE);
-        pathfinder = new AStarPathfinder(grid);
+    fun setUp() {
+        grid = AStarGrid(GRID_SIZE, GRID_SIZE)
+        pathfinder = AStarPathfinder(grid)
     }
 
     @Test
-    public void testFindPath() {
-        List<AStarCell> path = pathfinder.findPath(3, 0, 5, 0);
-        assertPathEquals(path, 4, 0, 5, 0);
+    fun testFindPath() {
+        var path = pathfinder.findPath(3, 0, 5, 0)
+        assertPathEquals(path, 4, 0, 5, 0)
 
         // Add barriers.
-        for (int i = 0; i <= 4; i++)
-            grid.get(4, i).setState(CellState.NOT_WALKABLE);
-        path = pathfinder.findPath(3, 0, 5, 0);
+        for (i in 0..4) grid[4, i].state = CellState.NOT_WALKABLE
+        path = pathfinder.findPath(3, 0, 5, 0)
         assertPathEquals(path,
                 3, 1,
                 3, 2,
@@ -52,26 +40,23 @@ public class AStarPathfinderTest {
                 5, 3,
                 5, 2,
                 5, 1,
-                5, 0);
+                5, 0)
 
         // Make passing impossible.
-        for (int i = 0; i <= 19; i++)
-            grid.get(4, i).setState(CellState.NOT_WALKABLE);
-        path = pathfinder.findPath(3, 0, 5, 0);
-        assertTrue(path.isEmpty());
+        for (i in 0..19) grid[4, i].state = CellState.NOT_WALKABLE
+        path = pathfinder.findPath(3, 0, 5, 0)
+        assert(path.isEmpty())
     }
 
     @Test
-    public void testFindPathWithBusyCells() {
-        grid.get(3, 0).setState(CellState.NOT_WALKABLE);
-        grid.get(3, 1).setState(CellState.NOT_WALKABLE);
-        grid.get(3, 2).setState(CellState.NOT_WALKABLE);
-        grid.get(3, 3).setState(CellState.NOT_WALKABLE);
-        grid.get(3, 5).setState(CellState.NOT_WALKABLE);
-        grid.get(1, 4).setState(CellState.NOT_WALKABLE);
-
-
-        List<AStarCell> path = pathfinder.findPath(1, 1, 4, 5, new ArrayList<>());
+    fun testFindPathWithBusyCells() {
+        grid[3, 0].state = CellState.NOT_WALKABLE
+        grid[3, 1].state = CellState.NOT_WALKABLE
+        grid[3, 2].state = CellState.NOT_WALKABLE
+        grid[3, 3].state = CellState.NOT_WALKABLE
+        grid[3, 5].state = CellState.NOT_WALKABLE
+        grid[1, 4].state = CellState.NOT_WALKABLE
+        val path = pathfinder.findPath(1, 1, 4, 5, ArrayList())
         assertPathEquals(path,
                 2, 1,
                 2, 2,
@@ -79,8 +64,8 @@ public class AStarPathfinderTest {
                 2, 4,
                 3, 4,
                 4, 4,
-                4, 5);
-        List<AStarCell> pathWithBusyCell = pathfinder.findPath(1, 1, 4, 5, Collections.singletonList(grid.get(3, 4)));
+                4, 5)
+        val pathWithBusyCell = pathfinder.findPath(1, 1, 4, 5, listOf(grid[3, 4]))
         assertPathEquals(pathWithBusyCell,
                 2, 1,
                 2, 2,
@@ -90,22 +75,22 @@ public class AStarPathfinderTest {
                 2, 6,
                 3, 6,
                 4, 6,
-                4, 5);
+                4, 5)
     }
 
-    private void assertPathEquals(List<AStarCell> path, int... points) {
-        assertEquals(points.length / 2, path.size(), reportNotMatchingPaths(path, points));
-
-        int i = 0;
-        for (AStarCell cell : path) {
-            assertEquals(points[i++], cell.getX(), reportNotMatchingPaths(path, points));
-            assertEquals(points[i++], cell.getY(), reportNotMatchingPaths(path, points));
+    private fun assertPathEquals(path: List<AStarCell>, vararg points: Int) {
+        val pointsList = points.toList().chunked(2) { it[0] to it[1] }
+        assert(pointsList.size == path.size) { reportNotMatchingPaths(path, pointsList) }
+        pointsList.zip(path).forEach { (point, cell) ->
+            assert(point.first == cell.x) { reportNotMatchingPaths(path, pointsList) }
+            assert(point.second == cell.y) { reportNotMatchingPaths(path, pointsList) }
         }
-
-        assertEquals(points.length, i, reportNotMatchingPaths(path, points));
     }
 
-    private Supplier<String> reportNotMatchingPaths(List<AStarCell> path, int... points){
-        return () -> "Paths do not match: \n" + path + "\n != \n" + Arrays.toString(points);
+    private fun reportNotMatchingPaths(path: List<AStarCell>, points: List<Pair<Int, Int>>): String =
+            "Paths do not match: \n$path \n!=\n${points}"
+
+    companion object {
+        private const val GRID_SIZE = 20
     }
 }
