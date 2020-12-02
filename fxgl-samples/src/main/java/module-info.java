b4b3b5eebf50e6d13f930d38dfb304cf8162093a
Almas Baimagambetov <almaslvl@gmail.com>
2020-12-02 09:11:53 +0000

    added a comment to clarify why a module is required to be separate from fxgl.all

--- fxgl-samples/src/main/java/module-info.java
+++ fxgl-samples/src/main/java/module-info.java
@@ -8,8 +8,9 @@ open module samples.main {
     requires kotlin.stdlib;
     requires com.almasb.fxgl.all;
 
-    // TODO: tmp test
-    requires com.almasb.fxgl.controllerinput;
-
     requires java.desktop;
+
+    // these modules are not part of fxgl.all since they are use case specific
+    // these modules can be used as a separate dependency, e.g. in Maven
+    requires com.almasb.fxgl.controllerinput;
 }
