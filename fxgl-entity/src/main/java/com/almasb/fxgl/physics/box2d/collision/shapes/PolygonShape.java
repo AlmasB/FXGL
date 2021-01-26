291a5153e0db6882f82f9e0110c8ce6e2c4b7484
Almas Baimagambetov <almaslvl@gmail.com>
2021-01-26 11:12:39 +0000

    removed unused method: validate()

--- fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/shapes/PolygonShape.java
+++ fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/shapes/PolygonShape.java
@@ -587,34 +587,6 @@ public final class PolygonShape extends Shape {
         out.mulLocal(1.0f / area);
     }
 
-    /**
-     * Validate convexity. This is a very time consuming operation.
-     *
-     * @return
-     */
-    public boolean validate() {
-        for (int i = 0; i < vertexCount; ++i) {
-            int i1 = i;
-            int i2 = i < vertexCount - 1 ? i1 + 1 : 0;
-            Vec2 p = m_vertices[i1];
-            Vec2 e = pool1.set(m_vertices[i2]).subLocal(p);
-
-            for (int j = 0; j < vertexCount; ++j) {
-                if (j == i1 || j == i2) {
-                    continue;
-                }
-
-                Vec2 v = pool2.set(m_vertices[j]).subLocal(p);
-                float c = Vec2.cross(e, v);
-                if (c < 0.0f) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
     /** Get the vertices in local coordinates. */
     public Vec2[] getVertices() {
         return m_vertices;
