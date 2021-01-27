7ce5d13eee2eb6ca98a35e61047c28fe0b8f9d46
Almas Baimagambetov <almaslvl@gmail.com>
2021-01-27 13:38:07 +0000

    removed unused method getSupportVertex()

--- fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/Distance.java
+++ fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/Distance.java
@@ -539,26 +539,6 @@ public class Distance {
         }
 
         /**
-         * Get the supporting vertex in the given direction.
-         *
-         * @param d
-         * @return
-         */
-        public final Vec2 getSupportVertex(final Vec2 d) {
-            int bestIndex = 0;
-            float bestValue = Vec2.dot(m_vertices[0], d);
-            for (int i = 1; i < m_count; i++) {
-                float value = Vec2.dot(m_vertices[i], d);
-                if (value > bestValue) {
-                    bestIndex = i;
-                    bestValue = value;
-                }
-            }
-
-            return m_vertices[bestIndex];
-        }
-
-        /**
          * Get the vertex count.
          *
          * @return
