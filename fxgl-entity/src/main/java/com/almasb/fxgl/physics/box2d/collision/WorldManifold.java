52e1814a4a90898b930351fc10bc77fa0ee675b5
Almas Baimagambetov <almaslvl@gmail.com>
2021-01-27 21:15:04 +0000

    fix PMD violations

--- fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/WorldManifold.java
+++ fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/WorldManifold.java
@@ -7,7 +7,6 @@ package com.almasb.fxgl.physics.box2d.collision;
 
 import com.almasb.fxgl.core.math.Vec2;
 import com.almasb.fxgl.physics.box2d.common.JBoxSettings;
-import com.almasb.fxgl.physics.box2d.common.JBoxUtils;
 import com.almasb.fxgl.physics.box2d.common.Rotation;
 import com.almasb.fxgl.physics.box2d.common.Transform;
 
