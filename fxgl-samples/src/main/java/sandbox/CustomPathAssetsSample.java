a2ea995aaba6a46c9f041a2ef0d0b1d10f6ab664
Almas Baimagambetov <almaslvl@gmail.com>
2021-02-19 08:57:32 +0000

    added sample for #967

---
+++ fxgl-samples/src/main/java/sandbox/CustomPathAssetsSample.java
@@ -0,0 +1,33 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package sandbox;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public class CustomPathAssetsSample extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {
+
+    }
+
+    @Override
+    protected void initGame() {
+        entityBuilder()
+                .view("testcoin.png")
+                .buildAndAttach();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
