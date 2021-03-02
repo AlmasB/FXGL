1d3abd6de56208e4115e42269e7d2510ff858922
Almas Baimagambetov <almaslvl@gmail.com>
2021-03-02 11:05:54 +0000

    refactor

--- fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/dynamics/ContactManager.java
+++ fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/dynamics/ContactManager.java
@@ -52,16 +52,10 @@ class ContactManager implements PairCallback {
     }
 
     private void addType(IDynamicStack<Contact> creator, ShapeType type1, ShapeType type2) {
-        ContactRegister register = new ContactRegister();
-        register.creator = creator;
-        register.primary = true;
-        contactStacks[type1.ordinal()][type2.ordinal()] = register;
+        contactStacks[type1.ordinal()][type2.ordinal()] = new ContactRegister(creator, true);
 
         if (type1 != type2) {
-            ContactRegister register2 = new ContactRegister();
-            register2.creator = creator;
-            register2.primary = false;
-            contactStacks[type2.ordinal()][type1.ordinal()] = register2;
+            contactStacks[type2.ordinal()][type1.ordinal()] = new ContactRegister(creator, false);
         }
     }
 
@@ -350,7 +344,12 @@ class ContactManager implements PairCallback {
     }
 
     private static class ContactRegister {
-        IDynamicStack<Contact> creator;
-        boolean primary;
+        final IDynamicStack<Contact> creator;
+        final boolean primary;
+
+        ContactRegister(IDynamicStack<Contact> creator, boolean primary) {
+            this.creator = creator;
+            this.primary = primary;
+        }
     }
 }
