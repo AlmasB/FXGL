69b1702eeb5095dd12e80841c25a40db2fc582d7
Almas Baimagambetov <almaslvl@gmail.com>
2021-04-01 20:58:58 +0100

    refactor

--- fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/shapes/ChainShape.java
+++ fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/shapes/ChainShape.java
@@ -126,32 +126,22 @@ public final class ChainShape extends Shape {
 
         edge.setRadius(getRadius());
 
-        final Vec2 v0 = m_vertices[index + 0];
-        final Vec2 v1 = m_vertices[index + 1];
-        edge.m_vertex1.x = v0.x;
-        edge.m_vertex1.y = v0.y;
-        edge.m_vertex2.x = v1.x;
-        edge.m_vertex2.y = v1.y;
+        edge.m_vertex1.set(m_vertices[index + 0]);
+        edge.m_vertex2.set(m_vertices[index + 1]);
 
         if (index > 0) {
-            Vec2 v = m_vertices[index - 1];
-            edge.m_vertex0.x = v.x;
-            edge.m_vertex0.y = v.y;
+            edge.m_vertex0.set(m_vertices[index - 1]);
             edge.m_hasVertex0 = true;
         } else {
-            edge.m_vertex0.x = m_prevVertex.x;
-            edge.m_vertex0.y = m_prevVertex.y;
+            edge.m_vertex0.set(m_prevVertex);
             edge.m_hasVertex0 = m_hasPrevVertex;
         }
 
         if (index < m_count - 2) {
-            Vec2 v = m_vertices[index + 2];
-            edge.m_vertex3.x = v.x;
-            edge.m_vertex3.y = v.y;
+            edge.m_vertex3.set(m_vertices[index + 2]);
             edge.m_hasVertex3 = true;
         } else {
-            edge.m_vertex3.x = m_nextVertex.x;
-            edge.m_vertex3.y = m_nextVertex.y;
+            edge.m_vertex3.set(m_nextVertex);
             edge.m_hasVertex3 = m_hasNextVertex;
         }
     }
