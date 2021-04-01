2c02436305175ab4c357f0f1c13025b7e7e0519d
Almas Baimagambetov <almaslvl@gmail.com>
2021-04-01 20:54:07 +0100

    refactor

--- fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/shapes/ChainShape.java
+++ fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/collision/shapes/ChainShape.java
@@ -72,21 +72,15 @@ public final class ChainShape extends Shape {
 
     @Override
     public boolean raycast(RayCastOutput output, RayCastInput input, Transform xf, int childIndex) {
-        assert childIndex < m_count;
-
-        final EdgeShape edgeShape = pool0;
-
         int i1 = childIndex;
         int i2 = childIndex + 1;
         if (i2 == m_count) {
             i2 = 0;
         }
-        Vec2 v = m_vertices[i1];
-        edgeShape.m_vertex1.x = v.x;
-        edgeShape.m_vertex1.y = v.y;
-        Vec2 v1 = m_vertices[i2];
-        edgeShape.m_vertex2.x = v1.x;
-        edgeShape.m_vertex2.y = v1.y;
+
+        EdgeShape edgeShape = pool0;
+        edgeShape.m_vertex1.set(m_vertices[i1]);
+        edgeShape.m_vertex2.set(m_vertices[i2]);
 
         return edgeShape.raycast(output, input, xf, 0);
     }
