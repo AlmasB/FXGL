5772909ac13c9ddfa3e8f9edd32d526f8a05263e
Almas Baimagambetov <almaslvl@gmail.com>
2021-04-12 17:14:46 +0100

    added more examples of image resize

--- fxgl-samples/src/main/java/intermediate/DifferentSizeTextureSample.java
+++ fxgl-samples/src/main/java/intermediate/DifferentSizeTextureSample.java
@@ -8,6 +8,8 @@ package intermediate;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.texture.ImagesKt;
+import com.almasb.fxgl.texture.Texture;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
 
@@ -26,16 +28,28 @@ public class DifferentSizeTextureSample extends GameApplication {
         // the actual size of texture brick is 64x64
 
         entityBuilder()
-                .at(200, 300)
+                .at(50, 500)
                 // 1. tell asset loader to load resized texture
                 .view(getAssetLoader().loadTexture("brick.png", 32, 32))
                 .buildAndAttach();
 
         entityBuilder()
-                .at(300, 300)
+                .at(150, 500)
                 // you can also load with different ratio
                 .view(getAssetLoader().loadTexture("brick.png", 64, 32))
                 .buildAndAttach();
+
+        // you can also resize textures after loading
+
+        var original = image("brick.png");
+        for (int i = 1; i < 12; i++) {
+            var resized = ImagesKt.resize(original, 16 * i, 16 * i);
+
+            entityBuilder()
+                    .at(5 * i  * i, 0.3 * i * i * i)
+                    .view(new Texture(resized))
+                    .buildAndAttach();
+        }
     }
 
     public static void main(String[] args) {
