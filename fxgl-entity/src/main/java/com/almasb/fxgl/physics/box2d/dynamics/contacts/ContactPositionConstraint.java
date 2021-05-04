094b1248749481ebe960fae309761aacc1ae05d6
Almas Baimagambetov <almaslvl@gmail.com>
2021-05-04 13:57:13 +0100

    refactor

--- fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/dynamics/contacts/ContactPositionConstraint.java
+++ fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/dynamics/contacts/ContactPositionConstraint.java
@@ -9,21 +9,24 @@ import com.almasb.fxgl.core.math.Vec2;
 import com.almasb.fxgl.physics.box2d.collision.Manifold.ManifoldType;
 import com.almasb.fxgl.physics.box2d.common.JBoxSettings;
 
-public class ContactPositionConstraint {
+class ContactPositionConstraint {
     Vec2[] localPoints = new Vec2[JBoxSettings.maxManifoldPoints];
     final Vec2 localNormal = new Vec2();
     final Vec2 localPoint = new Vec2();
     int indexA;
     int indexB;
-    float invMassA, invMassB;
+    float invMassA;
+    float invMassB;
     final Vec2 localCenterA = new Vec2();
     final Vec2 localCenterB = new Vec2();
-    float invIA, invIB;
+    float invIA;
+    float invIB;
     ManifoldType type;
-    float radiusA, radiusB;
+    float radiusA;
+    float radiusB;
     int pointCount;
 
-    public ContactPositionConstraint() {
+    ContactPositionConstraint() {
         for (int i = 0; i < localPoints.length; i++) {
             localPoints[i] = new Vec2();
         }
