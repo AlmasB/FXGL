ee22ae8de3175c3555a3f6f51bbfc1a1822b3560
Almas Baimagambetov <almaslvl@gmail.com>
2021-05-07 18:59:42 +0100

    added missing test case

--- fxgl-entity/src/test/kotlin/com/almasb/fxgl/entity/components/CollidableComponentTest.kt
+++ fxgl-entity/src/test/kotlin/com/almasb/fxgl/entity/components/CollidableComponentTest.kt
@@ -3,10 +3,11 @@
  * Copyright (c) AlmasB (almaslvl@gmail.com).
  * See LICENSE for details.
  */
-
+@file:Suppress("JAVA_MODULE_DOES_NOT_DEPEND_ON_MODULE")
 package com.almasb.fxgl.entity.components
 
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.contains
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -31,5 +32,17 @@ class CollidableComponentTest {
 
         c.removeIgnoredType(MyType.ONE)
         assertTrue(c.getIgnoredTypes().isEmpty())
+    }
+
+    @Test
+    fun `Copy`() {
+        val c = CollidableComponent(true)
+        c.addIgnoredType(MyType.ONE)
+        c.addIgnoredType(MyType.TWO)
+
+        val c2 = c.copy()
+
+        assertThat(c2.getIgnoredTypes(), contains<Serializable>(MyType.ONE, MyType.TWO))
+        assertThat(c2.value, `is`(c.value))
     }
 }
