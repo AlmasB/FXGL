ff53585997202eeaa6f125a19ea6e340a6ba3d6b
Almas Baimagambetov <almaslvl@gmail.com>
2021-07-20 20:10:27 +0100

    editor experiments

--- fxgl-samples/src/main/java/sandbox/CustomComponent.java
+++ fxgl-samples/src/main/java/sandbox/CustomComponent.java
@@ -6,8 +6,14 @@
 
 package sandbox;
 
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.dsl.components.OffscreenCleanComponent;
+import com.almasb.fxgl.dsl.components.ProjectileComponent;
 import com.almasb.fxgl.entity.component.Component;
 import com.almasb.fxgl.entity.component.CopyableComponent;
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 /**
  * @author Almas Baimagambetov (almaslvl@gmail.com)
@@ -22,5 +28,14 @@ public class CustomComponent extends Component implements CopyableComponent<Cust
     @Override
     public CustomComponent copy() {
         return new CustomComponent();
+    }
+
+    public void exampleMethod() {
+        FXGL.entityBuilder()
+                .at(200, 200)
+                .view(new Rectangle(60, 40, Color.RED))
+                .with(new ProjectileComponent(new Point2D(1, 0), 150))
+                .with(new OffscreenCleanComponent())
+                .buildAndAttach();
     }
 }
