938daa1f33eaa524eb028525abe7eced67cf7b91
Almas Baimagambetov <almaslvl@gmail.com>
2021-09-29 20:15:42 +0100

    clean up

--- fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/dynamics/joints/FrictionJointDef.java
+++ fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/dynamics/joints/FrictionJointDef.java
@@ -3,9 +3,7 @@
  * Copyright (c) AlmasB (almaslvl@gmail.com).
  * See LICENSE for details.
  */
-/**
- * Created at 7:23:39 AM Jan 20, 2011
- */
+
 package com.almasb.fxgl.physics.box2d.dynamics.joints;
 
 import com.almasb.fxgl.core.math.Vec2;
@@ -19,33 +17,25 @@ import com.almasb.fxgl.physics.box2d.dynamics.World;
  */
 public class FrictionJointDef extends JointDef<FrictionJoint> {
 
-
     /**
      * The local anchor point relative to bodyA's origin.
      */
-    public final Vec2 localAnchorA;
+    public final Vec2 localAnchorA = new Vec2();
 
     /**
      * The local anchor point relative to bodyB's origin.
      */
-    public final Vec2 localAnchorB;
+    public final Vec2 localAnchorB = new Vec2();
 
     /**
      * The maximum friction force in N.
      */
-    public float maxForce;
+    public float maxForce = 0f;
 
     /**
      * The maximum friction torque in N-m.
      */
-    public float maxTorque;
-
-    public FrictionJointDef() {
-        localAnchorA = new Vec2();
-        localAnchorB = new Vec2();
-        maxForce = 0f;
-        maxTorque = 0f;
-    }
+    public float maxTorque = 0f;
 
     /**
      * Initialize the bodies, anchors, axis, and reference angle using the world anchor and world
