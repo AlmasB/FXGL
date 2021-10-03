fd725cc778ea8b296f1a73d2d2a8ad2ff174ad0b
Almas Baimagambetov <almaslvl@gmail.com>
2021-10-03 20:17:30 +0100

    clean up

--- fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/dynamics/joints/DistanceJointDef.java
+++ fxgl-entity/src/main/java/com/almasb/fxgl/physics/box2d/dynamics/joints/DistanceJointDef.java
@@ -5,18 +5,18 @@
  */
 /*
  * JBox2D - A Java Port of Erin Catto's Box2D
- * 
+ *
  * JBox2D homepage: http://jbox2d.sourceforge.net/
  * Box2D homepage: http://www.box2d.org
- * 
+ *
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the authors be held liable for any damages
  * arising from the use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  * 1. The origin of this software must not be misrepresented; you must not
  * claim that you wrote the original software. If you use this software
  * in a product, an acknowledgment in the product documentation would be
@@ -32,52 +32,48 @@ import com.almasb.fxgl.core.math.Vec2;
 import com.almasb.fxgl.physics.box2d.dynamics.Body;
 import com.almasb.fxgl.physics.box2d.dynamics.World;
 
-//Updated to rev 56->130->142 of b2DistanceJoint.cpp/.h
-
 /**
- * Distance joint definition. This requires defining an anchor point on both bodies and the non-zero
- * length of the distance joint. The definition uses local anchor points so that the initial
- * configuration can violate the constraint slightly. This helps when saving and loading a game.
- *
- * @warning Do not use a zero or short length.
+ * Distance joint definition.
+ * This requires defining an anchor point on both bodies and the non-zero length of the distance joint.
+ * The definition uses local anchor points so that the initial configuration can violate the constraint slightly.
+ * This helps when saving and loading a game.
+ * Do not use a zero or short length.
  */
 public class DistanceJointDef extends JointDef<DistanceJoint> {
-    /** The local anchor point relative to body1's origin. */
-    public final Vec2 localAnchorA;
 
-    /** The local anchor point relative to body2's origin. */
-    public final Vec2 localAnchorB;
+    /**
+     * The local anchor point relative to body1's origin.
+     */
+    public final Vec2 localAnchorA = new Vec2();
 
-    /** The equilibrium length between the anchor points. */
-    public float length;
+    /**
+     * The local anchor point relative to body2's origin.
+     */
+    public final Vec2 localAnchorB = new Vec2();
+
+    /**
+     * The equilibrium length between the anchor points.
+     */
+    public float length = 1.0f;
 
     /**
      * The mass-spring-damper frequency in Hertz.
      */
-    public float frequencyHz;
+    public float frequencyHz = 0.0f;
 
     /**
-     * The damping ratio. 0 = no damping, 1 = critical damping.
+     * The damping ratio.
+     * 0 = no damping, 1 = critical damping.
      */
-    public float dampingRatio;
-
-    public DistanceJointDef() {
-        localAnchorA = new Vec2(0.0f, 0.0f);
-        localAnchorB = new Vec2(0.0f, 0.0f);
-        length = 1.0f;
-        frequencyHz = 0.0f;
-        dampingRatio = 0.0f;
-    }
+    public float dampingRatio = 0.0f;
 
     /**
      * Initialize the bodies, anchors, and length using the world anchors.
      *
-     * @param b1 First body
-     * @param b2 Second body
      * @param anchor1 World anchor on first body
      * @param anchor2 World anchor on second body
      */
-    public void initialize(final Body b1, final Body b2, final Vec2 anchor1, final Vec2 anchor2) {
+    public void initialize(Body b1, Body b2, Vec2 anchor1, Vec2 anchor2) {
         setBodyA(b1);
         setBodyB(b2);
         localAnchorA.set(b1.getLocalPoint(anchor1));
