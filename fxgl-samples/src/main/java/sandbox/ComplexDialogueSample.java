27a2a9dd378ba6cb88770e3f21fc1fe5febb71de
Almas Baimagambetov <almaslvl@gmail.com>
2021-11-15 13:43:34 +0000

    added complex dialogue sample

---
+++ fxgl-samples/src/main/java/sandbox/ComplexDialogueSample.java
@@ -0,0 +1,95 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
+package sandbox;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.core.collection.PropertyMap;
+import com.almasb.fxgl.cutscene.dialogue.DialogueContext;
+import com.almasb.fxgl.cutscene.dialogue.FunctionCallHandler;
+import com.almasb.fxgl.dsl.components.view.TextViewComponent;
+import com.almasb.fxgl.entity.Entity;
+import javafx.scene.input.KeyCode;
+import javafx.scene.shape.Rectangle;
+
+import java.util.Map;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+/**
+ * Shows how to use in-game dialogues.
+ */
+public class ComplexDialogueSample extends GameApplication {
+
+    private Entity entity;
+
+    @Override
+    protected void initSettings(GameSettings settings) { }
+
+    @Override
+    protected void initInput() {
+        // simple dialogue
+        onKeyDown(KeyCode.Q, () -> {
+            // the json file is loaded from /assets/dialogues/
+            var dialogueGraph = getAssetLoader().loadDialogueGraph("simple.json");
+
+            getCutsceneService().startDialogueScene(dialogueGraph);
+        });
+
+        onKeyDown(KeyCode.F, () -> {
+            System.out.println("Global: " + getWorldProperties());
+            System.out.println("Local: " + entity.getProperties());
+        });
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("playerName", "The One");
+    }
+
+    @Override
+    protected void initGame() {
+        entity = entityBuilder()
+                .at(200, 200)
+                .view(new Rectangle(50, 50))
+                .with(new TextViewComponent(0, 63, "Placeholder NPC"))
+                .with("hasMet", false)
+                .onClick(e -> {
+                    // the json file is loaded from /assets/dialogues/
+                    var dialogueGraph = getAssetLoader().loadDialogueGraph("example_dialogue.json");
+
+                    getCutsceneService().startDialogueScene(dialogueGraph, new ExampleContext(e));
+                })
+                .buildAndAttach();
+    }
+
+    private static class ExampleContext implements DialogueContext {
+
+        private Entity e;
+
+        ExampleContext(Entity e) {
+            this.e = e;
+        }
+
+        @Override
+        public PropertyMap properties() {
+            return e.getProperties();
+        }
+    }
+
+    private static class ExampleFunctionHandler implements FunctionCallHandler {
+
+        @Override
+        public Object handle(String functionName, String[] args) {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
