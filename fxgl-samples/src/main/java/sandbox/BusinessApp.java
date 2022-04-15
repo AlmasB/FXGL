12e2cb4664a9ae93ff58eb9e3837ac5eeea381c7
Almas Baimagambetov <almaslvl@gmail.com>
2022-04-15 17:31:13 +0100

    remove unused import

--- fxgl-samples/src/main/java/sandbox/BusinessApp.java
+++ fxgl-samples/src/main/java/sandbox/BusinessApp.java
@@ -10,7 +10,6 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
