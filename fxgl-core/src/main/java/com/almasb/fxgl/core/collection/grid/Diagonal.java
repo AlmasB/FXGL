bc0181cafaedea82a6ff2d02ca24144b5e6a8248
Jean-René Lavoie <>
2024-01-19 15:07:44 -0500

    docs: Adding required license header

--- fxgl-core/src/main/java/com/almasb/fxgl/core/collection/grid/Diagonal.java
+++ fxgl-core/src/main/java/com/almasb/fxgl/core/collection/grid/Diagonal.java
@@ -1,5 +1,17 @@
+/*
+ * FXGL - JavaFX Game Library. The MIT License (MIT).
+ * Copyright (c) AlmasB (almaslvl@gmail.com).
+ * See LICENSE for details.
+ */
+
 package com.almasb.fxgl.core.collection.grid;
 
+
+/**
+ * Define Diagonal Movement mode
+ *
+ * @author Jean-Rene Lavoie (jeanrlavoie@gmail.com)
+ */
 public enum Diagonal {
 
     NEVER, ALLOWED;
