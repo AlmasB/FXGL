7ba0d74ee3e5ced52a7d650162047a5c8a237be2
Aaron Corky <aaron.corky@gmail.com>
2026-03-01 23:03:01 +0100

    refactor: improve TODO comments in UDP client/server and NetServiceTest

--- fxgl-io/src/main/kotlin/com/almasb/fxgl/net/udp/UDPServer.kt
+++ fxgl-io/src/main/kotlin/com/almasb/fxgl/net/udp/UDPServer.kt
@@ -84,7 +84,9 @@ class UDPServer<T>(val port: Int, private val config: UDPServerConfig<T>) : Serv
         onStoppedListening()
     }
 
-    // TODO: extract into common between TCPServer
+    /**
+     * Extracted logic could be shared with TCPServer for common server stop handling.
+     */
     override fun stop() {
         if (isStopped) {
             log.warning("Attempted to stop a server that is already stopped")
