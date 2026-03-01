2d2f655b8cc4b7d69d2044e0effc537e57598947
Aaron Corky <aaron.corky@gmail.com>
2026-03-01 22:56:19 +0100

    refactor(PropertyViews): improve read-only property detection
    Replace string-based class name checking with proper type checking:
    - Use 'property is Property<*>' instead of checking canonicalName
    - More robust and type-safe approach
    
    Fixes TODO in PropertyViews.kt:39

--- fxgl-scene/src/main/kotlin/com/almasb/fxgl/ui/property/PropertyViews.kt
+++ fxgl-scene/src/main/kotlin/com/almasb/fxgl/ui/property/PropertyViews.kt
@@ -36,8 +36,8 @@ import javafx.util.converter.IntegerStringConverter
 class DoublePropertyView(property: ObservableDoubleValue) : TextField() {
 
     init {
-        // TODO: any other way to check if read-only?
-        if (!property.javaClass.canonicalName.contains("ReadOnlyDoubleWrapper")) {
+        // Check if property is writable (Property) or read-only (ObservableValue only)
+        if (property is Property<*>) {
             textProperty().bindBidirectional(property as Property<Double>, DoubleStringConverter())
         } else {
             textProperty().bind((property as DoubleExpression).asString())
@@ -50,7 +50,7 @@ class DoublePropertyView(property: ObservableDoubleValue) : TextField() {
 class IntPropertyView(property: ObservableIntegerValue) : TextField() {
 
     init {
-        if (!property.javaClass.canonicalName.contains("ReadOnlyIntegerWrapper")) {
+        if (property is Property<*>) {
             textProperty().bindBidirectional(property as Property<Int>, IntegerStringConverter())
         } else {
             textProperty().bind((property as IntegerExpression).asString())
